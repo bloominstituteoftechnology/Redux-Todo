180b6a1325baebfe09984b0d2b67f6cdb565edd0
mixelpixel <mixelpix@mac.com>
2017-07-27 18:32:17 -0400

    todo list reducer setup

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import todoListReducer from './todoListReducer'
+// import addTodoItem from './addTodoItem'
 
 const rootReducer = combineReducers({
-
+  todoList: todoListReducer,
+  // addTodoItem: addTodoItem
 });
 
 export default rootReducer;
