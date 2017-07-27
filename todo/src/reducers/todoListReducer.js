a17167cd3c60bfc9c46354ca2ec37eeb8bdad42c
mixelpixel <mixelpix@mac.com>
2017-07-27 18:38:00 -0400

    starter todo list

---
+++ todo/src/reducers/todoListReducer.js
@@ -0,0 +1,21 @@
+export default () => {
+  // console.log('>>>>>>>REDUCER: src/reducers/todoListReducer.js')
+  return [
+    {
+      id: 1,
+      todoText: 'wash car.',
+      completed: false
+    },
+    {
+      id: 2,
+      todoText: 'polish MyLittlePony statues.',
+      completed: false
+
+    },
+    {
+      id: 3,
+      todoText: 'finish LS-React project.',
+      completed: false
+    }
+  ]
+}
