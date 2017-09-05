8a0d2d76909387b0c837e74eff160e298cf7e88a
EricB <ericblancas23@gmail.com>
2017-09-05 10:39:11 -0700

    files changes

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import './index.css';
+import todoApp from './reducers';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(todoApp);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+registerServiceWorker();
