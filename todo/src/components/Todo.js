8c330266d963ef1085fadfa8d7ba2adcc76b7ffc
ubuntu <irishseanvaldivia@gmail.com>
2017-12-12 12:14:23 -0600

    Almost there...

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -2,4 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Todo from './Todo';
 
-const Todos = ()
+const Todos = (props) => {
+    const todos = props.todos.map((todo, i) => <Todo todo={todo} key={i} index={i} />);
+    return (
+        <ul>
+          { todos }
+        </ul>
+    );
+}
+
+export default connect(mapStateToProps)(Todos);
