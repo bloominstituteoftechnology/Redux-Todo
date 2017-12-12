63ffe41a6fa56d0c3cad65a01fb451b9f6bbf105
Yasin Shuman <email@yasin.io>
2017-12-12 10:56:58 -0800

    tried to follow along. still dont know what im doing

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducers from "./reducers";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
