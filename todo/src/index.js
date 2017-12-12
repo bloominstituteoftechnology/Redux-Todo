0ae96e855ea0360fc6e1fb3b8c0753ef7d633f77
ubuntu <irishseanvaldivia@gmail.com>
2017-12-12 11:45:23 -0600

    Learning more.

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-let store = createStore(todoApp)
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
