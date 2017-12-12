eeb6729ca29a88e5fa4ed68bcd128f6f17dbd49d
ubuntu <irishseanvaldivia@gmail.com>
2017-12-12 12:30:43 -0600

    Just when I thought Ivan couldn't type any faster lol!

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -5,6 +5,22 @@ import { toggleComplete } from '../actions';
 
 class Todo extends Component {
 
+    toggleComplete() {
+        this.props.toggleComplete(this.props.index);
+    }
+    
+    render() {
+        return (
+            <li>
+              <input
+                type="checkbox"
+                onChange={this.toggleComplete.bind(this)}
+                />
+                {this.props.todo.value}
+            </li>
+        );
+    }
+
 }
 
 export default connect(null, { toggleComplete })(Todo);
