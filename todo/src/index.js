b45b74abc065023bf3408d0904d38382d37377e3
ubuntu <irishseanvaldivia@gmail.com>
2017-12-12 12:04:11 -0600

    Just another commit.

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 import './index.css';
 import App from './App';
 
+const store = createStore(reducers);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={createStore(reducers)}>
+  <App />
+</Provider>,
+document.getElementById('root'));
+
+
