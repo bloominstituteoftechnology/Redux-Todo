e6936e8496106fc28f4d085b9b9fd2394ca85b85
Aaron Wilder <element1028@gmail.com>
2017-12-11 21:29:37 -0800

    cleaning up

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import reducers from './reducers';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
