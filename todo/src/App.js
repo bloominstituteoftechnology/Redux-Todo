a90e24ceab3c361e950269d60b25dcf8b3c6bca1
ubuntu <irishseanvaldivia@gmail.com>
2017-12-12 12:11:25 -0600

    It will sink in soon.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import Todos from './components/Todos';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+            <TodoForm />
+            <Todos />
             </div>
         );
     }
---
+++ todo/src/components/Todo.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Todo from './Todo';
+
+const Todos = ()
