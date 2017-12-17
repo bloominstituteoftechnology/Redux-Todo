004de9f54109b2b17f3ca721f9d4d18126c962a4
ubuntu <irishseanvaldivia@gmail.com>
2017-12-17 13:24:55 -0600

    Got it working!

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,15 @@
-import { combineReducers } from 'redux';
-import todosReducers from './reducers/todosReducers';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 
-const rootReducer = combineReducers({
-  todos: todosReducers
-});
-
-export default rootReducer;
+ReactDOM.render(
+  <Provider store={createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    <App />
+  </Provider>, document.getElementById('root'));
+  
+registerServiceWorker();
