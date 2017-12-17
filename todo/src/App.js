4a8abe3292caf5e2b94cbe53b38d8c22cb55e8c9
ubuntu <irishseanvaldivia@gmail.com>
2017-12-17 12:37:57 -0600

    Let's get this party rolling!

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import TodoForm from './components/TodoForm';
-import Todos from './components/Todos';
+import { connect } from 'react-redux';
+import { addTodo } from './actions';
+import TodoList from './components/TodoList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodo: ''
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.updateNewTodo = this.updateNewTodo.bind(this);
+  }
+
+  addTodo(event) {
+    event.preventDefault();
+    this.props.addTodo({
+      value: this.state.newTodo,
+      complete: false
+    });
+    this.setState({
+      newTodo: ''
+    });
+  }
+
+  updateNewTodo(event) {
+    this.setState({
+      newTodo: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <TodoForm />
-        <Todos />
+        <form onSubmit={this.addTodo}>
+          <input
+            onChange={this.updateNewTodo}
+            placeholder="new todo"
+            value={this.state.newTodo}
+          />
+        </form>
+        <TodoList todos={this.props.todos} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos
+  };
+};
+
+export default connect(mapStateToProps, { addTodo })(App);
