04c0d6e99beb16d6218473edc6ebfa8fe9597889
ubuntu <irishseanvaldivia@gmail.com>
2017-12-17 12:34:24 -0600

    Getting closer...

--- todo/src/reducers/todosReducers.js
+++ todo/src/reducers/todosReducers.js
@@ -1,17 +1,16 @@
 import {
-    NEW_TODO,
-    TOGGLE_COMPLETE
-  } from '../actions';
-  
-  export default (todos = [], action) => {
-    switch (action.type) {
-      case NEW_TODO:
-        return todos.concat(action.payload);
-      case TOGGLE_COMPLETE:
-        const newTodos = todos.slice(0);
-        newTodos[action.payload].completed = !newTodos[action.payload].completed;
-        return newTodos
-      default:
-        return todos;
-    }
+  NEW_TODO,
+  TOGGLE_COMPLETE
+ } from '../actions';
+
+export default (todos = [], action) => {
+  switch (action.type) {
+    case NEW_TODO:
+      return todos.concat(action.payload);
+    case TOGGLE_COMPLETE:
+      todos[action.payload].complete = !todos[action.payload].complete;
+      return todos;
+    default:
+      return todos;
   }
+};
