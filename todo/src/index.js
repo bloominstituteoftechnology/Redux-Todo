8ac7b3af9c472c758824ef5611c45b1bb3683de4
Mephestys <mephestys@gmail.com>
2018-02-20 17:30:50 -0700

    Changes in this commit: - Removed logo.svg amd registerServiceWorker.js - Created actions.js and reducers.js in appropriate dirs. - Created store in index.js - ADD_TODO and TOGGLE_TODO added to reducers.js

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import todos from './reducers/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css';
+
+const store = createStore(todos);
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  document.getElementById('root')
+);
