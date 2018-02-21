4b4e1c6e2d0051c46cc3df7936a5cad3b28a47b2
Christopher Beards <beardscm@gmail.com>
2018-02-21 10:42:36 -0500

    Starting Over

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers/index';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import reducer from './reducers'
 
-const store = createStore(reducers);
-//create your store in `src/index.js`.  The `<Provider >` component will wrap `<App />` 
-//and you will pass the created store into `<Provider >` as one of its properties.
-ReactDOM.render(
-<Provider store={store}>
-  <App />
-</Provider>,
-document.getElementById('root'));
+const store = createStore(reducer)
 
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
