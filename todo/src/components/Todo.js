c6c58d2556c0b21957e4a307780be5860ea59c40
Christopher Beards <beardscm@gmail.com>
2018-02-21 11:41:37 -0500

    Todo Component Created

---
+++ todo/src/components/Todo.js
@@ -0,0 +1,22 @@
+//It is easier to define presentational components as functional components
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Todo = ({ onClick, completed, text }) => (
+  <li
+  onClick={onClick}
+  style={ {
+    textDecoration: completed? 'line-through' : 'none'
+  }}
+  >
+    {text}
+  </li>
+)
+
+Todo.propTyles = {
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired
+}
+
+export default Todo;
