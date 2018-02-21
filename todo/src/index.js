7013f8438347e10668a90de958d837ddbe58cc44
Ronnie Miksch <fakepreferences@gmail.com>
2018-02-20 16:01:37 -0800

    Boilerplate

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import todo from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(todo);
+
+ReactDOM.render(
+      <Provider store={store} >
+        <App />
+      </Provider>
+      , document.getElementById('root'));
 registerServiceWorker();
