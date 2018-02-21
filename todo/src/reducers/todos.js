16c560940aeff43e661d73822a0e47b338fac9d8
Dixie Korley <dixiekorley@gmail.com>
2018-02-21 18:31:17 -0500

    Changing Todo.js

--- todo/src/reducers/todos.js
+++ todo/src/reducers/todos.js
@@ -1,11 +1,22 @@
+import { ADD_TODO, TOGGLE_TODO } from '../actions';
+
 export default (state = [], action) => {
     switch (action.type) {
-        case :
-
-        case :
-
-        case :
-
+        case ADD_TODO:
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ]
+        case TOGGLE_TODO:
+            return state.map(todo =>
+                (todo.id === action.id)
+                ? {...todo, completed: !todo.completed}
+                : todo
+            )
         default:
             return state;
     }
