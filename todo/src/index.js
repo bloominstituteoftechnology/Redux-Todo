f9844f5952b8b09c2df9abd8fc59045526600f8b
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-02-21 15:26:02 -0500

    First Commit of Re-Do

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,17 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import Todo from "./components/Todo.js";
-
-import todo from "./reducers/index.js";
-
-const store = createStore(todo);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Todo />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
