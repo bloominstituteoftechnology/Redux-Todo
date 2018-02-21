057a91459fe2be43912e21c29c25f8b60dfaef8a
David Loveday <davemech@yahoo.com>
2018-02-20 17:11:24 -0800

    created todo folder which contains other folders and files that are not completely implemented

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,15 @@
+import { ADDTODO, TOGGLE } from '../actions';
+
+
+export default (todos = [], action) => {
+    switch (action.type) {
+        case ADDTODO:
+
+            return todos;
+        case TOGGLE:
+            
+            return todos;
+        default:
+            return todos;
+    }
+};
