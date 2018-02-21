ed218b869a063427f66084014b406779e1477310
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-02-21 15:55:09 -0500

    Render List
    ToDo.js --- Rendered the basics of the to-do list, no real functionality.
    
    reducers/index.js --- Made state initially be an array with an object instead of an array that just holds strings of the todo.

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -3,10 +3,44 @@ import { connect } from "react-redux";
 import {} from "../actions/index.js";
 
 class ToDo extends React.Component {
-  render() {
-    return <p>Yup</p>;
-  }
+    state = {
+        todo: "",
+        completed: false
+      };
+    
+      render() {
+        console.log("Props: ", this.props);
+        return (
+          <div>
+            <h1> To Do List </h1>
+            <ul>
+              {this.props.todos.map((item, index) => {
+                return
+                  <div>
+                    <li> {item.todo} </li>
+                  </div>
+              })}
+            </ul>
+            <input
+              type="text"
+              name="todo"
+              value={this.state.todo}
+              placeholder="Enter ToDo Here"
+              onChange={this.handleInputChange}
+            />
+            <button onClick={this.handleClick}>Add To Do to the List</button>
+          </div>
+        );
+      }
+      handleInputChange = event => {
+        const todo = event.target.value;
+        this.setState({ todo: todo });
+      };
+      handleClick = () => {
+        this.setState({ todo: "" });
+      };
 }
+
 
 const mapStateToProps = state => {
   console.log("State: ", state);
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { } from "../actions";
 
-export default (todos = [], action) => {
+export default (todos = [{key: 0, todo: '', completed: false}], action) => {
   console.log("Action: ", action);
   switch (action.type) {
     default:
