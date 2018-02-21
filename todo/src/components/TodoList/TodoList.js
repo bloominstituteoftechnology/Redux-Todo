0c20465a722f1e4955abbdcdbe08955949692310
Unknown <peter@petergraycreative.com>
2018-02-21 15:54:24 -0600

    Add form and handlesubmit

--- todo/src/components/TodoList/TodoList.js
+++ todo/src/components/TodoList/TodoList.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addTodo, todoCompleted } from '../../actions/actions'
+import { addTodo, todoCompleted } from '../../actions/actions';
 
 class TodoList extends Component {
   state = {
-    todo: '', 
-  }
+    todo: '',
+  };
   handleInputChange = event => {
     this.setState({ todo: event.target.value });
     console.log(event.target.value);
   };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    
+  }
   render() {
     return (
       <div>
         <ul>
           {this.props.todos.map((todo, i) => {
-            return <li key={i}>{todo}</li>
+            return <li key={i}>{todo}</li>;
           })}
         </ul>
-        <input onChange={this.handleInputChange} />
-        <button>Submit</button>
+        <form onSubmit={this.props.addTodo}>
+          <input onChange={this.handleInputChange} />
+          <button onClick={}>Submit</button>
+        </form>
       </div>
     );
   }
