0d0dc4393f2d477b7085765e49c8f3cb5703544f
Christopher Beards <beardscm@gmail.com>
2018-02-21 18:36:08 -0500

    App is Functional

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,26 +1,23 @@
-//Creates a counter for each Todo ID
-let nextTodoId = 0
-
-//ACTIONS -> Send Data from app to store using dispatch
-//Action Creators -> Create Actions
-export const addTodo = (text) => ({
-  type: 'ADD_TODO',
-  id: nextTodoId++,
-  text
-})
-
-export const setVisibilityFilter = (filter) => ({
-  type: 'SET_VISIBILITY_FILTER',
-  filter
-})
-
-export const toggleTodo = (id) => ({
-  type: 'TOGGLE_TODO',
-  id
-})
-
+//ACTIONS -> Send Data from app to store using dispatch 
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+ 
 export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
   SHOW_COMPLETED: 'SHOW_COMPLETED',
   SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+  
+//Action Creators -> Create Actions
+export function addTodo(text) {
+  return { type: ADD_TODO, text }
+}
+ 
+export function toggleTodo(index) {
+  return { type: TOGGLE_TODO, index }
+}
+ 
+export function setVisibilityFilter(filter) {
+  return { type: SET_VISIBILITY_FILTER, filter }
 }
