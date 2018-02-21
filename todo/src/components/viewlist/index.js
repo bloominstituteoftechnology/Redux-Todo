14c9517f0cf9bb05ce85fc5d04fa3ef9865b0524
Christopher Beards <beardscm@gmail.com>
2018-02-20 20:16:09 -0500

    ViewList Component

---
+++ todo/src/components/viewlist/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class ViewList extends React.Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Todos</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    );
+  }
+}
