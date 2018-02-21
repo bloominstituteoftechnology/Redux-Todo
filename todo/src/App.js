61cf7c8085e207700464fa4c0f75358b79526da9
Christopher Beards <beardscm@gmail.com>
2018-02-20 20:22:33 -0500

    Components Display Properly

--- todo/src/App.js
+++ todo/src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Hey! What's Up?</h1>
-        </header>
+      <Header />
+      <TodoForm />
+      <ViewList />
       </div>
     );
   }
