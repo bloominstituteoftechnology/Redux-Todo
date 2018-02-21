489d8bf204bdf00d4686546ccf3ea3e8efa3548a
Unknown <peter@petergraycreative.com>
2018-02-21 12:01:30 -0600

    Add basic render function, mapped state to props, and connected to store

---
+++ todo/src/components/TodoList/TodoList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class TodoForm extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.props.todos.map(todo => {
+            <li >{this.todo}</li>
+          })}
+        </ul>
+        <input />
+        <button>Submit</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    todos: [state],
+  };
+};
+
+export default connect(mapStateToProps)(TodoForm);
