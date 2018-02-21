00536312f2e592496107029231f51ae35c30f034
Unknown <peter@petergraycreative.com>
2018-02-21 11:51:52 -0600

    Add Todo actions
    Added addTodo and todoCompleted actions. todoCompleted access the todo by index. Should later generate a unique key for the Todo item.

---
+++ todo/src/actions/actions.js
@@ -0,0 +1,15 @@
+export const ADD_TODO = 'ADD_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+
+export const addTodo = () => {
+    return {
+        type: ADD_TODO,
+        text
+    }
+}
+export const todoCompleted = (index) => {
+    return {
+        type: COMPLETE_TODO,
+        index
+    }
+}
