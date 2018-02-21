6a32937eeecfe25acb558c674fb73eb994b40bc4
lambda <lambda@aegis.local>
2018-02-20 18:14:03 -0600

    Commit 1: /src/index.js: - removed unnecessary imports - removed registerServiceWorker from ReactDOM - added 'store' var *note: The first commit went through incorrectly, ignore initial commit

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(todo);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root'));
