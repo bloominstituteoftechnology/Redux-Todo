c373fd8a57ea156c81c13a8f05f0991485dc5225
Unknown <peter@petergraycreative.com>
2018-02-22 09:31:57 -0600

    Small syntax changes

--- todo/src/actions/actions.js
+++ todo/src/actions/actions.js
@@ -1,16 +1,16 @@
 export const ADD_TODO = 'ADD_TODO';
 export const COMPLETE_TODO = 'COMPLETE_TODO';
 
-export const addTodo = text => {
+export const addTodo = (text) => {
     return {
         type: ADD_TODO,
-        text
+        text,
     }
 };
 
 export const todoCompleted = (index) => {
     return {
         type: COMPLETE_TODO,
-        index
+        index: index,
     }
 };
