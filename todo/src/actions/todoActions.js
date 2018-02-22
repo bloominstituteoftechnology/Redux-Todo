79207d36467a05829f7626c105f469a4ea06592d
algorithmsrock <algorithmsrock@gmail.com>
2018-02-22 09:00:25 -0800

    Redux completed with stretch problems completed

--- todo/src/actions/todoActions.js
+++ todo/src/actions/todoActions.js
@@ -1,12 +1,21 @@
 export const ADDTODO = 'ADDTODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
 export const TOOGLE_COMPLETE = 'TOOGLE_COMPLETE';
+export const IMPORT_STATE = 'IMPORT_STATE';
 
-export const addTodo = todo => {
+export const addTodo = (todo) => {
 	return {
       type: ADDTODO,
 			payload: todo				
   	};
 };
+
+export const removeTodo = (id) => {
+	 return {
+      type: 'REMOVE_TODO',
+		  payload: id,
+	 };
+};	 
 
 export const toggleComplete = (id) => {
 	return {
@@ -14,3 +23,9 @@ export const toggleComplete = (id) => {
 		 payload: id				 
 	};
 };
+
+export const importState = () => {
+	return {
+    type: 'IMPORT_STATE',
+	}
+}	
--- todo/src/containers/TodoListContainer.js
+++ todo/src/containers/TodoListContainer.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addTodo, toggleComplete } from '../actions/todoActions';
+import { addTodo, removeTodo, toggleComplete, importState } from '../actions/todoActions';
 
 class Todo extends Component {
    state = {
       value: '',
 			completed: false,			 
   	}
+
+	constructor(props) {
+		super(props);
+		this.props.importState();
+	}	
 
   handleInput = (event) => {
 		  this.setState({ value: event.target.value });
@@ -18,6 +23,10 @@ addTodoHandler = (event) => {
 	this.setState({ value: '', completed: false });
 };
 
+removeHandler = (event) => {
+	 this.props.removeTodo(event.target.getAttribute('id'));
+}	
+
 toggleHandler = (event) => {
     this.props.toggleComplete(event.target.getAttribute('id'));
 }
@@ -28,14 +37,19 @@ render() {
 		      <h1>To do list</h1>
 				   <form onSubmit={this.addTodoHandler}>
 					<input id='input' onChange={this.handleInput} name='value' value={this.state.value}/>
+					&ensp;
 					<input type='submit' value='Add Task' onClick={this.addTodoHandler} />
         </form>
         <ul>
           {this.props.todos.map((todo, i) => {
             return (
+							<div>	
               <li key={i} id={i} className={todo.completed ? 'todo-completed' : ''} onClick={this.toggleHandler}>
                 {todo.value}
               </li>
+							&ensp;
+							<button key={i} id={i} onClick={this.removeHandler}>x</button>
+							</div>
             );
           })}
         </ul>
@@ -50,5 +64,5 @@ const mapStateToProps = (state) => {
 	 };
 }
 
-export default connect(mapStateToProps, { addTodo, toggleComplete })(Todo); 
+export default connect(mapStateToProps, { addTodo, removeTodo, toggleComplete, importState })(Todo); 
 
