d81ed58e0c673c49caf074c1e3fb488db997a653
shobanavv <shobanavv@gmail.com>
2018-02-22 08:06:00 -0800

    conceptual understanding but still coding

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider} from 'react-redux';
 import Todo from './components/Todo';
-import todo from './reducers';
+import todoApp from './reducers';
 
-const store = createStore(listItem);
-
+const store = createStore(todoApp);
+// Store receives reducer's state and modifies state and rerenders.
 ReactDOM.render(
     <Provider store={store}> 
         <Todo />
---
+++ todo/src/reducers/index.js
@@ -0,0 +1,3 @@
+//Reducer gets state and action function and decides 
+//what should be the state based on typeof action.
+//returns changed state to store. 
