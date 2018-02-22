f24eadeb1f42a1a22a1fc8f3da39993320a486cc
Unknown <peter@petergraycreative.com>
2018-02-22 09:38:33 -0600

    Change ADD_TODO reducer to return array that includes previous state
    Set todoState default to an array.
    
    Changed COMPLETE_TODO to map over todos array rather than state.

--- todo/src/reducers/reducers.js
+++ todo/src/reducers/reducers.js
@@ -1,21 +1,19 @@
 import { ADD_TODO, COMPLETE_TODO } from '../actions/actions';
 
-export default (todoState = null, action) => {
+export default (todoState = [], action) => {
   console.log('The Reducer Ran');
   switch (action.type) {
     case ADD_TODO:
-      return Object.assign({}, todoState, {
-        todos: [
-          ...todoState,
-          {
-            text: action.text,
-            completed: false,
-          },
-        ],
-      });
+      return [
+        ...todoState,
+        {
+          text: action.text,
+          completed: false,
+        },
+      ]
     case COMPLETE_TODO:
       return Object.assign({}, todoState, {
-        todos: todoState.todos.map((todo, index) => {
+        todos: todoState.map((todo, index) => {
           if (index === action.index) {
             return Object.assign({}, todo, {
               completed: !todo.completed,
