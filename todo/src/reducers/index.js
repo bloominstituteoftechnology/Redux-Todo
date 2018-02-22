8c5deac708369bfae58f9a7397ba54adf82b9ff9
Mephestys <mephestys@gmail.com>
2018-02-22 11:35:45 -0700

    TOGGLE_TODO now maps todos rather than directly mutating todos

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -6,8 +6,10 @@ const todoReducer = (todos = [], action) => {
       //return todos.concat(action.payload);
       return [...todos, action.payload];
     case TOGGLE_TODO:
-      todos[action.payload].complete = !todos[action.payload].complete;
-      return todos;
+      // todos[action.payload].complete = !todos[action.payload].complete;
+      return todos.map(todo => (todo.id === action.id) 
+        ? {...todo, completed: 
+        !todo.completed} : todo);
     case DELETE_TODO:
       const newTodos = todos.slice(0);
       newTodos.splice(action.payload, 1);
