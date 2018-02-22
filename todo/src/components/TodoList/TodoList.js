2137d1bdd1a0e1490f5347b8a93b1f2c4b3eb067
Unknown <peter@petergraycreative.com>
2018-02-22 13:29:34 -0600

    Changed handleSubmit to pass entire object payload to action
    Reset state to '' onSubmit
    
    Realized my map function was trying to output 'todo' in the <li> which is an object rather than todo.value which is the property.

--- todo/src/components/TodoList/TodoList.js
+++ todo/src/components/TodoList/TodoList.js
@@ -6,25 +6,44 @@ class TodoList extends Component {
   state = {
     text: '',
   };
+  //Handles change from input and stores in this.state
   handleInputChange = event => {
     this.setState({ text: event.target.value });
-    console.log(this.state);
+    // console.log(this.state);
   };
+
+  //Submits action payload, resets state onSubmit
   handleSubmit = event => {
-    event.preventDefault();    
-    this.props.addTodo(this.state.text);
+    event.preventDefault();
+    this.props.addTodo({
+      value: this.state.text,
+      completed: false,
+    });
+    this.setState({
+      text: '',
+    });
   };
+
   render() {
     return (
       <div>
-      { this.props.todos.length > 0 ? console.log('something here') : console.log('nothing here')}
+        {/* { this.props.todos.length > 0 ? console.log('something here') : console.log('nothing here')}
+      {console.log(this.props.todos)} */}
         <ul>
           {this.props.todos.map((todo, i) => {
-            return <li key={i}>{todo}</li>;
+            return (
+              <li key={i} index={i}>
+                {todo.value}
+              </li>
+            );
           })}
         </ul>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" onChange={this.handleInputChange}/>
+          <input
+            type="text"
+            onChange={this.handleInputChange}
+            value={this.state.text}
+          />
           <button type="submit">Submit</button>
         </form>
       </div>
