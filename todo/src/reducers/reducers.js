da0ae855e29f8fbf4d033cdbe6dfeb43ee8f5e99
Unknown <peter@petergraycreative.com>
2018-02-22 13:27:51 -0600

    Made inital state more clear as 'todos', rewrote ADD_TODO
    Took in payload to ADD_TODO rather than creating an object.
    
    Changed COMPLETE_TODO and default to match state name.
    
    Added return todos; rather than return todo in COMPLETE_TODO

--- todo/src/reducers/reducers.js
+++ todo/src/reducers/reducers.js
@@ -1,28 +1,22 @@
 import { ADD_TODO, COMPLETE_TODO } from '../actions/actions';
 
-export default (todoState = [], action) => {
+export default (todos = [], action) => {
   console.log('The Reducer Ran');
   switch (action.type) {
     case ADD_TODO:
-      return [
-        ...todoState,
-        {
-          text: action.text,
-          completed: false,
-        },
-      ]
+      return [...todos, action.payload];
     case COMPLETE_TODO:
-      return Object.assign({}, todoState, {
-        todos: todoState.map((todo, index) => {
+      return Object.assign({}, todos, {
+        todos: todos.map((todo, index) => {
           if (index === action.index) {
             return Object.assign({}, todo, {
               completed: !todo.completed,
             });
           }
-          return todo;
+          return todos;
         }),
       });
     default:
-      return todoState;
+      return todos;
   }
 };
