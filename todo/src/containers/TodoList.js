d6fe01729f445db2df0eee6192414fdd26185911
Carlos Andrade <carlos.andrade@outlook.com>
2018-03-20 17:12:07 -0400

    WIP

---
+++ todo/src/containers/TodoList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+
+
+
+
+
+render() {
+    return (
+        <div>
+            <form onSubmit={}>
+            <input
+                type="text"
+                onChange={}
+                placeholder="Add a new item"
+                value={}
+                />
+            </form>
+          
+        </div>
+    );
+}
+}
+
+export default TodoList;
+
