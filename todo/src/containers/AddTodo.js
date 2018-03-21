5e162e6e71668f8fc5e929790c02022485c982bc
Jeremy Jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-03-20 17:35:57 -0700

    Basic todo list working

---
+++ todo/src/containers/AddTodo.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addTodo } from '../actions';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: ${props => (props.primary ? 'palevioletred' : 'white')};
+  color: ${props => (props.primary ? 'white' : 'palevioletred')};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: palevioletred;
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+`;
+
+let AddTodo = ({ dispatch }) => {
+  let input;
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return;
+          }
+          dispatch(addTodo(input.value));
+          input.value = '';
+        }}
+      >
+        <input
+          ref={node => {
+            input = node;
+          }}
+        />
+        <Button type="submit">Add Todo</Button>
+      </form>
+    </div>
+  );
+};
+
+AddTodo = connect()(AddTodo);
+
+export default AddTodo;
