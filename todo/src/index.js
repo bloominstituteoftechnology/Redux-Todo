911a61618501b2bf1006120e6a0e3317e7dd7a4c
Johnathan Huggett <johnathanhuggett@Johnathans-MacBook-Pro.local>
2018-03-21 12:37:13 -0400

    Johnathan Huggett, working on stretch

---
+++ todo/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import todos from './reducers'
+import './index.css';
+import App from './App';
+
+const store = createStore(todos)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
