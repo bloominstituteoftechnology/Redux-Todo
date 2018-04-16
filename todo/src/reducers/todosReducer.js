c9af9baec94164c2f5909619b2846326667a1404
Justin Hong <jhong2009@gmail.com>
2018-04-16 14:56:53 -0700

    in components dir: created component files. In todosReducer: fixed syntax error

--- todo/src/reducers/todosReducer.js
+++ todo/src/reducers/todosReducer.js
@@ -1,10 +1,13 @@
-export default function todosReducer(state={
-	todos: [],
-}, action) {
-	switch(action.type) {
-		case "NEWTODO"; {
+export default function todosReducer(
+	state = {
+		todos: []
+	},
+	action
+) {
+	switch (action.type) {
+		case "NEWTODO": {
 			// update store with the new todo
-			return {...state.todos.concat(action.payload)};
+			return { ...state.todos.concat(action.payload) };
 		}
 		case "TOGGLETODO": {
 			// change isComplete to the opposite value
