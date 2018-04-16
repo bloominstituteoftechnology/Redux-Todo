53b62f08c00135101a4f0498e80ec46406538089
Justin Hong <jhong2009@gmail.com>
2018-04-16 14:54:14 -0700

    in todosReducer.js: added logic to switch() git push

--- todo/src/reducers/todosReducer.js
+++ todo/src/reducers/todosReducer.js
@@ -4,9 +4,13 @@ export default function todosReducer(state={
 	switch(action.type) {
 		case "NEWTODO"; {
 			// update store with the new todo
+			return {...state.todos.concat(action.payload)};
 		}
 		case "TOGGLETODO": {
 			// change isComplete to the opposite value
+			return !action.payload.isComplete;
 		}
+		default:
+			return state;
 	}
 }
