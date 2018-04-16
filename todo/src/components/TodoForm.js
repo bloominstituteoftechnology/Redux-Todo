6c210c03a14edeb62ad94d33326e1ad0fbb3304f
Justin Hong <jhong2009@gmail.com>
2018-04-16 15:30:06 -0700

    in TodoForm.js: set up class component. in App.js: imported TodoForm. in index.js: fixed provider import statement

---
+++ todo/src/components/TodoForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>form here</h1>
+			</div>
+		);
+	}
+}
+
+export default TodoForm;
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
-import { Provider } from "redux";
+import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
