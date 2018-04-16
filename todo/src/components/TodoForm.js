1c6afebe78b8c084fb11fd3232c5695d818682b9
Justin Hong <jhong2009@gmail.com>
2018-04-16 16:34:20 -0700

    in TodoForm.js: added form with input field and submit btn with onClick handler

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,17 +1,50 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addTodoAction } from "../actions/addTodoAction";
 
 class TodoForm extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			newTodo: ""
+		};
+	}
+
+	handleNewTodo(event) {
+		this.state.newTodo = event.target.value;
 	}
 
 	render() {
+		// console.log(this.state.value);
 		return (
 			<div>
 				<h1>form here</h1>
+				<form>
+					<input
+						type="text"
+						placeholder="Type your Todos Here"
+						name="newTodo"
+						value={this.state.newTodo}
+						onChange={this.handleNewTodo}
+					/>
+					<button
+						type="button"
+						onClick={() => {
+							this.props.addTodoAction();
+						}}
+					>
+						Add Todo
+					</button>
+				</form>
 			</div>
 		);
 	}
 }
 
-export default TodoForm;
+const mapStateToProps = state => {
+	return {
+		todos: state
+	};
+};
+
+export default connect(mapStateToProps, { addTodoAction })(TodoForm);
