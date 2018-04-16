af753e9b79a5b153a03b8e1b35e4b2a7e911092b
JacobLeonlyerla <umasour@yahoo.com>
2018-04-16 16:02:56 -0700

    still setting things up but it's moving along

---
+++ todo/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import todoList from './reducers/reducer';
+import './index.css';
+import App from './components/App';
+
+const store =createStore(todoList);
+
+ReactDOM.render(
+    <Provider store={store}>
+<App />
+</Provider>,
+ document.getElementById('root'));
+
---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,11 @@
+import {ADD} from '../actions'
+
+export default (action) =>{
+  switch(1){
+    case ADD:
+    //what it should do
+      return;
+    default:
+     return;
+  }
+}
