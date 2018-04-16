c75e051ac63057045f193b3591549895ad37cd5c
Justin Hong <jhong2009@gmail.com>
2018-04-16 14:30:01 -0700

    in index.js: set up store and wrapped App with Provider

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "redux";
+
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+	todosReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
 registerServiceWorker();
