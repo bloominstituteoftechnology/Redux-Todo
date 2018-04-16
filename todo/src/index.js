91c8c96188622cfb2a2b1dde9b6e38f6ed0d8d6d
2940cristian <2940cristian@gmail.com>
2018-04-16 14:29:46 -0700

    ininitial commit + folder structure

---
+++ todo/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from 'redux';
+import registerServiceWorker from './registerServiceWorker';
+import reducers from "./reducers"
+
+const store = createStore(reducers)
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
+registerServiceWorker();
