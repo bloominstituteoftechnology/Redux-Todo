d519758c8ab98e5ccface60b4c47e1de3f3a9bd4
Justin M <jekm321@gmail.com>
2018-04-17 19:50:41 -0400

    redux webkit works now :D and some css

--- todo/src/index.js
+++ todo/src/index.js
@@ -10,8 +10,8 @@ import App from './App';
 
 const store = createStore(
     reducer,
-    localState()
-    // +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    localState(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 store.subscribe(() => {
