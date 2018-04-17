5f6dd5c81d5421c5e6de67b76a8ecbc1738680b6
Justin M <jekm321@gmail.com>
2018-04-17 15:18:52 -0400

    data persistence is implemented and works, both saves and loads

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { reducer } from './reducers'
+import { localState, saveState } from './persistence'
 
 import './index.css';
 import App from './App';
 
+const load = localState();
 const store = createStore(
     reducer,
+    load
     // +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+    saveState(store.getState());
+})
 
 ReactDOM.render(
     <Provider store={store}>
