579577c522407e44eff48f963368c905d6c2a982
Paul Fang <paulfang@pauls-mbp.home>
2018-04-17 14:38:42 -0700

    working on ToDo list

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TodoList from './component/TodoList';
+import AddTodo from './component/AddTodo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TodoList />
-        <TodoInput />
+        <AddTodo />
       </div>
     );
   }
