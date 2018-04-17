a134de148a6139c351979c068ace1c7076c8268b
cgoez <goez2001@gmail.com>
2018-04-17 12:26:09 -0700

    Setup src index.js, Redux store, imports

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
+// initialize Redux store
+// pass reducer to createStore function
+const store = createStore(todo);
+
+// wrap React component with Provider
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
 );
 registerServiceWorker();
-
-const store = createStore(todo)
