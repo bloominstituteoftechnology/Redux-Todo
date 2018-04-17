35fe93ee437cc9111a0a7437f525afdf75942bce
Nedu Robert <nedumrobert@gmail.com>
2018-04-18 00:09:08 +0100

    added actions functionality

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,8 +1,16 @@
 export const ADDTODO = 'ADDTODO';
+export const DELETETODO = 'DELETETODO';
 
 export const addTodo = (todo) => {
     return {
-        type: 'ADDTODO',
+        type: ADDTODO,
         payload: todo
+    }
+}
+
+export const deleteTodo = (id) => {
+    return {
+        type: DELETETODO,
+        payload: id
     }
 }
