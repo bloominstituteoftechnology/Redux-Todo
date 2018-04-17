470cb40c6d67587471505772a61d2b4a8f28a861
Natalie Schennum <natalieschennum@gmail.com>
2018-04-17 13:43:52 -0700

    Setting Up Display, Components, and Actions

--- todo/todo/src/App.js
+++ todo/todo/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import DisplayTodos from './components/DisplayTodos';
 import './App.css';
 import { connect } from 'react-redux';
 
@@ -8,12 +8,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Your Todo List</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <DisplayTodos/>
       </div>
     );
   }
