2323d07f139fe8bf92cd967a761997136cea3b3c
imsdhk <jappleleaf@gmail.com>
2018-04-17 16:13:55 -0400

    setup initial store

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Reducer from './reducer';
+
+// This line instantiates our central Redux store.
+// The `createStore` function receives the reducer
+// that is responsible for updating the store, along
+// with any initial state that we may want the store
+// to start out with (which is none in this case).
+
+
+// Here, we wrap our main React component inside of
+// Provider tags, which come from the react-redux package.
+// This is needed because the store needs to know where it
+// is passing its state to. The Provider component is also
+// where the store "lives".
+const store = createStore(Reducer);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+
+
+
 registerServiceWorker();
