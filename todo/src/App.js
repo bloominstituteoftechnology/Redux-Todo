140916ea4940374b5f5f8f0a215dcb9430063dd6
Paul Fang <paulfang@pauls-mbp.home>
2018-04-17 16:31:23 -0700

    fixed imports

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import AddTodo from "./containers/addTodo";
+import AddTodo from "./component/addTodo";
 
 class App extends Component {
   render() {
