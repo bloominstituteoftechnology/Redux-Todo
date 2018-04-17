149721e73738c582043181efb3a435fed3e97976
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-04-17 14:16:16 -0700

    Created files for TodoActions, Container & Reducers

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './containers/TodoListContainer';
+// Creating container for Todo List
 
 class App extends Component {
   render() {
@@ -8,11 +10,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Redux ToDo List</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
