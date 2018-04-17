fad6d245901bac5a663059602912358cb64c1591
Justin Hong <jhong2009@gmail.com>
2018-04-17 12:29:01 -0700

    in addTodoAction.js: modified action creator to accept text as parameter  and set payload to text

--- todo/src/actions/addTodoAction.js
+++ todo/src/actions/addTodoAction.js
@@ -1,9 +1,9 @@
 const NEWTODO = "NEWTODO";
 
-export function addTodoAction() {
+export function addTodoAction(text) {
 	// send new Todo data to todosReducer
 	return {
 		type: NEWTODO,
-		payload: ""
+		payload: text
 	};
 }
