1827ddd632648fb6e0c5aa89ffa1e2389bd2d060
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-17 14:42:20 -0700

    added provider and imports to index.js

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Provider from 'react-redux';
+import CreateStore, { createStore } from 'redux';
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
