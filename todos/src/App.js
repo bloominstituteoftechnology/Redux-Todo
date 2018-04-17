bddeaddc405d6b5c961b4828e4aeb27af3acae7f
totsuya <supertatortots@gmail.com>
2018-04-17 16:14:14 -0400

    added a button and input field

--- todos/src/App.js
+++ todos/src/App.js
@@ -5,15 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="ToDoList">
+      <input placeholder='Add your To Dos here!'></input>
+      <button>Submit</button>
       </div>
+      // <div>
+      //   Add Task list here
+      // </div>
     );
   }
 }
