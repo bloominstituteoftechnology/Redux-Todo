eeffa2b346ccdca02917c6e8b24325d5b44f3fd3
totsuya <supertatortots@gmail.com>
2018-04-17 16:19:21 -0400

    added reducers import

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { submitToDo } from './reducers';
 
 class App extends Component {
   render() {
     return (
       <div className="ToDoList">
-      <input placeholder='Add your To Dos here!'></input>
+      <input type="text" placeholder='Add your To Dos here!'></input>
       <button>Submit</button>
       </div>
       // <div>
