c8e2bda4865e0da5935e89c9ebc4bf9c883496d2
Abraham Bueno <abueno@localhost.localdomain>
2018-04-18 00:15:41 -0500

    Fixed toggle functionality, no more bugs, toggling completion now works...

--- todo/src/components/container.js
+++ todo/src/components/container.js
@@ -4,12 +4,14 @@ import { add, remove } from '../actions/actions';
 
 let cstyle = {
 	// color: 'red',
-	cursor: 'pointer',
+	cursor: 'pointer'
 	// hover: {backgroundColor: 'yellow'}
 };
-let test = {};
+let test = {
+	color: [],
+};
 let bstyle = {
-	cursor: 'pointer',
+	cursor: 'pointer'
 	// width: '10%'
 };
 let lstyle = {
@@ -43,32 +45,44 @@ class List extends Component {
 			color: 'red',
 			cursor: 'pointer'
 		};
-		test = {
-			color: 'red',
-			cursor: 'pointer'
-		};
+		// test = {
+		// 	color: 'red',
+		// 	cursor: 'pointer'
+		// };
+		test.color[this.props.todos.length] = 'red'
 	};
-	completeI = (i) => {
-		if (test.color === 'blue') {
-			test = {
-				color: 'red',
-				cursor: 'pointer'
-			};
+	completeI = (i, item) => {
+		// for (let j = i; i >= j; j++) {
+		if (test.color[i] === 'blue') {
+			// test = {
+			// 	color: '',
+			// 	color: 'red',
+			// 	cursor: 'pointer'
+			// };
+			test.color[i] = 'red';
 			cindex[i] = 'false';
-		} else if (test.color === 'red') {
-			test = {
-				color: 'blue',
-				cursor: 'pointer'
-			};
+			this.setState({ completed: !this.state.completed });
+		} else if (test.color[i] === 'red') {
+			// test = {
+			// 	color: '',
+			// 	color: 'blue',
+			// 	cursor: 'pointer'
+			// };
+			test.color[i] = 'blue'
 			cindex[i] = 'true';
+			this.setState({ completed: !this.state.completed });
 		} else {
-			test = {
-				color: 'red',
-				cursor: 'pointer'
-			};
+			// test = {
+			// 	color: '',
+			// 	color: 'red',
+			// 	cursor: 'pointer'
+			// };
+			test.color[i] = 'red'
 			cindex[i] = 'false';
+			this.setState({ completed: !this.state.completed });
 		}
-		this.setState({ completed: !this.state.completed });
+		// }
+
 		console.log('cindex[i] boolean: ', this.state.cindex[i]);
 		console.log('color inside cindex, color: ', cstyle.color);
 	};
@@ -102,19 +116,20 @@ class List extends Component {
 						console.log('inside todos map of container', item);
 						return (
 							<div style={lstyle} key={item + i}>
-							<li  >
-								<h3>{item}</h3>
+								<li>
+									<h3>{item}</h3>
 								</li>
 								<div>
-								<button style={bstyle} onClick={() => this.removeItem(item)}>Remove Todo</button>
+									<button style={bstyle} onClick={() => this.removeItem(item)}>
+										Remove Todo
+									</button>
 								</div>
 								{/* <div> */}
 
-								<h3 style={cstyle} onClick={() => this.completeI(i)}>
-								Completed: {cindex[i]}, click if completion changed
+								<h3 style={cstyle} onClick={() => this.completeI(i, item)}>
+									Completed: {cindex[i]}, click if completion changed
 								</h3>
-								</div>
-							
+							</div>
 						);
 					})}
 				</ul>
