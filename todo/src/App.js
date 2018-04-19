c6a06f440d9e9945ca6e052128c6977b21f2b362
JPEmery <clearkeyemery@gmail.com>
2018-04-18 22:26:55 -0500

    JP - David: Got the state to render the todo values

--- todo/src/App.js
+++ todo/src/App.js
@@ -12,6 +12,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">{this.props.todos.value}</h1>
         </header>
+        {this.props.todos.map((todo, index) => {
+          return (
+            <div key={index}> {todo.value} </div>
+          )
+
+        } )}
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- todo/src/index.js
+++ todo/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import todolist from './components/todolist'
+import todolist from './components/todolist';
 import todos from './reducers/reducers';
 // import 
 //insert component for reducer
