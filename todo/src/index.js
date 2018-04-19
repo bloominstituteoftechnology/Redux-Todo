10a360ff2adceded1e1c75d7a10db673ecb9cd74
JPEmery <clearkeyemery@gmail.com>
2018-04-18 21:12:49 -0500

    JP + David: initial commit todo

---
+++ todo/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+//insert component for reducer
+//insert Reducer
+ 
+ReactDOM.render(
+
+
+<App />
+
+
+, document.getElementById('root'));
+registerServiceWorker();
