590f3bcf3e7942997e36333922359492db0e98f2
Anne Courtney <annecourtney_1@yahoo.com>
2018-05-14 18:42:45 -0500

    got base todo list working, still need to add redux functionality.

---
+++ todo/src/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { TodoList } from  './components/component';
+
+const App = () => (
+  <div>
+    <h2>To-do App</h2>
+  <TodoList />
+  </div>
+);
+
+export default App;
