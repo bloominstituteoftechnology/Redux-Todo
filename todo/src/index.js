37b5528e4c2c459e9a3de37a257a35e974c7b517
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-05-15 13:29:06 -0700

    made a few directories with index.js files

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import todo from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(todo)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
---
+++ todo/src/reducers/todo.js
@@ -0,0 +1,3 @@
+const todo = (state = [], action) {
+    
+}
