ef78c7aa18fabc5e36704316b7af600cd8133c54
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-15 17:39:32 -0400

    Create store and pass it to app using Provider

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
+import { taskReducer } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(taskReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root')
+);
