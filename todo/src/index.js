1860bf206ab69936ae5c7d60a329a6778b47deb0
Lavell Burton <taylormade800@gmail.com>
2018-05-15 12:04:49 -0700

    added provider to root app

--- todo/src/index.js
+++ todo/src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+
+const store = createStore(reducer);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
+    registerServiceWorker();
