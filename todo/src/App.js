213d1bd8a4849649f1624eb945ea4e99f599e475
CMVNK <Kopecky12112@gmail.com>
2018-05-14 17:03:49 -0700

    set  up project, render basic ToDoList component on App component
    don't know if it'll be set up that way in long run - but I just wanted to get something
    
    on screen that wasn't broken.
    
    Moved files around.

---
+++ todo/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ToDoList from './containers/ToDoList';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ToDoList />      
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ todo/src/containers/ToDoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class ToDoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div>I'm a div that contains a ToDo</div>
+          )
+    }
+}
+ 
+export default ToDoList;
+
+
