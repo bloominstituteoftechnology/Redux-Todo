8a65772da8ca221ba35cb4f28c136e95512dd6c1
Lavell Burton <taylormade800@gmail.com>
2018-05-15 13:52:08 -0700

    added code to actions and reducers

---
+++ todo/src/actions/index.js
@@ -0,0 +1,17 @@
+export const TODO_LIST = 'TODO_LIST';
+export const TODO_FORM = 'TODO_FORM';
+
+
+
+export const todoList = () => {
+    return {
+        type: TODO_LIST,
+    };
+}
+
+
+export const todoForm = () => {
+    return {
+        type: TODO_FORM,
+    };
+}
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,1 +1,12 @@
-    
+import { todo } from '../actions'; 
+
+export default (todo, action) => { 
+    switch (action.type) {
+        case todo: 
+        return (todo.action.payload)    
+        case todo2:
+        return (todo.action.payload)
+        default:
+            return todo;    
+    }
+}
