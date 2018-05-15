be21f17ff2deaa01fc23b4805d46a0d25f1bd758
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-15 17:16:58 -0400

    -updated Provider wrapper with store

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,20 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import reducers from './reducers';
 
 
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(
-    
-)
 
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Provider store={createStore(reducers)}><App /></Provider>, document.getElementById('root'));registerServiceWorker();
