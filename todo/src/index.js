5ab54a0be09bcf73a01b640bfcd850d88d5393a4
kammagik <kammagik@yahoo.com>
2018-05-15 14:38:07 -0700

    wrapped app in provider

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import toDo from './reducer/todo.js';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(toDo);
 
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
