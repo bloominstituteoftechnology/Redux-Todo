625cdbc6860f37db8fc0a533de36dceb69508a3e
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-15 17:06:58 -0400

    Define actions - actionTypes.js consolidates list of actions - action declarations in index.js (add task, toggle task, delete task)

---
+++ todo/src/actions/index.js
@@ -0,0 +1,22 @@
+import { ADD_TASK, TOGGLE_TASK, DELETE_TASK } from './actionTypes';
+
+export const addTask = task => {
+  return {
+    type: ADD_TASK,
+    payload: newTask
+  };
+};
+
+export const toggleTask = taskId => {
+  return {
+    type: TOGGLE_TASK,
+    payload: taskId
+  };
+};
+
+export const deleteTask = taskId => {
+  return {
+    type: DELETE_TASK,
+    payload: taskId
+  }
+}
