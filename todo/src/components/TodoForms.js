4c5ac170395b096ccedd676305f7db9935d2990a
Niloc92 <prestonresult@github.com>
2018-05-16 09:45:45 -0500

    ES5 bind to ES6 syntax in TodoForm

--- todo/src/components/TodoForms.js
+++ todo/src/components/TodoForms.js
@@ -5,16 +5,19 @@ import { addTodo } from '../actions';
 
 class TodoForm extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         newTodo: ''
+    //     };
+    //     this.addTodo = this.addTodo.bind(this);
+    //     this.updateNewTodo = this.updateNewTodo.bind(this);
+    // }
+    state = {
             newTodo: ''
         };
-        this.addTodo = this.addTodo.bind(this);
-        this.updateNewTodo = this.updateNewTodo.bind(this);
-    }
 
-    addTodo(event) {
+    addTodo=(event)=>{
         event.preventDefault();
         this.props.addTodo({
             value: this.state.newTodo,
@@ -25,7 +28,7 @@ class TodoForm extends Component {
         });
     }
 
-    updateNewTodo(event) {
+    updateNewTodo=(event) => {
         this.setState({
             newTodo: event.target.value
         });
