fd9a461096c78258db0c189b86a0ccaf49deb924
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-16 09:28:23 -0400

    - imported TodoList

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './containers/Todos';
+import TodoList from './containers/TodoList';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div>
         <Todos />
+        <TodoList />
       </div>
     );
   }
