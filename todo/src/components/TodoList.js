b713a61e481c551ca5f8475cc214c0748ee40560
AmyShackles <amyshackles@gmail.com>
2018-05-16 07:47:08 -0700

    Refactored to not use any classes

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,32 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { addToDo } from '../actions'
 
-class ToDoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            todo: [],
-            item: '',
-        };
-    }
-    handleChange = event => {
-        this.setState({ [event.target.name]: event.target.value});
-    };
-    
-    render() {
+const ToDoList = props => {
         return (
             <div>
-                <input
-                    name="item"
-                    onChange={this.handleChange}
-                    value={this.state.item}
-                    placeholder="Add a item"
-                    />
-                    <button value={this.state.item} type="submit" onClick={(event) => this.props.addToDo(event.target.value)}> Add Item </button>
+                <form onSubmit={(event) => { props.addToDo(this.input.value); event.preventDefault()}}>
+                    <label>
+                        Add Item:
+                    <input type="text" ref={(input) => this.input = input} />
+                    </label>
+                    <input type="submit" value="input" />
+                    </form>
             </div>
         )
     }
-}
 
 export default connect(null, { addToDo })(ToDoList)
