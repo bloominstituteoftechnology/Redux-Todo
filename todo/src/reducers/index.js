7b8136dbc08a4ac6596dbb3595c8bd1321c813fe
Brandon Benefield <bsquared18@gmail.com>
2018-05-15 20:02:01 -0400

    feat(reducers/index): adds a new key `completed` by default when a task is created the `completed` property will decide whether or not to add CSS `text-decoration: line-through` on a specific task

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -3,8 +3,8 @@ import { ADD_TODO } from '../actions';
 export const todoReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      const todoArr = [{ title: action.payload, completed: false, ID: Date.now() }];
-      return state.concat(todoArr);
+      state = [{ ID: Date.now(), title: action.payload, completed: false }];
+      return state;
 
     default:
       return state;
