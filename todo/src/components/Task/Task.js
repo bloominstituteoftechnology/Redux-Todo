3d678e5be94517fc8aa0da9b1096565e0daef147
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-16 03:02:14 -0400

    Hook up delete task functionality to Task component

--- todo/src/components/Task/Task.js
+++ todo/src/components/Task/Task.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { ListGroupItem } from 'reactstrap';
 
-import { toggleTask } from '../../actions';
+import { toggleTask, deleteTask } from '../../actions';
 
 import './Task.css';
 
 const Task = props => {
   return (
-    <ListGroupItem
+    <ListGroupItem 
       className={(props.task.completed) ? 'complete' : 'incomplete'}
       data-id={`${props.task.id}`}
       onClick={() => props.toggleTask(props.task.id)}
     >
       <i className="fas fa-check-circle"></i>
       {props.task.value}
+      <button className="delete" onClick={() => props.deleteTask(props.task.id)}>
+        <i className="fas fa-trash-alt"></i>
+      </button>
     </ListGroupItem>
   )
 }
  
-export default connect(null, { toggleTask })(Task);
+export default connect(null, { toggleTask, deleteTask })(Task);
