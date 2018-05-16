7b9c18ac4993dccdd92a101a6e459ab50dcdb64b
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-17 02:11:01 +0700

    added localStorage to project to keep data when refreshing

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -2,11 +2,23 @@ import { ADD_TODO, COMPLETE_TODO, REMOVE_TODO } from '../actions';
 
 // { todoText: 'Stuff', completed: false}
 
-export default (state = [], action) => {
+let initialArray = [];
+
+const saveData = (array) => {
+    return localStorage.setItem('array', JSON.stringify(array));
+}
+
+const retrieveData = () => {
+    initialArray = JSON.parse(localStorage.getItem('array'));
+    return initialArray === null ? [] : initialArray;
+}
+
+export default (state = retrieveData(), action) => {
     switch (action.type) {
         case ADD_TODO:
-            const stateCopy = Array.from(state);
+            const stateCopy = Array.from(state);;
             stateCopy.push(action.payload);
+            saveData(stateCopy);
             return stateCopy;
         case COMPLETE_TODO:
             const stateCopy2 = Array.from(state);
@@ -15,10 +27,12 @@ export default (state = [], action) => {
             } else {
                 stateCopy2[action.payload].completed = true;
             }
+            saveData(stateCopy2);
             return stateCopy2;
         case REMOVE_TODO:
             const stateCopy3 = Array.from(state);
             stateCopy3.splice(action.payload, 1);
+            saveData(stateCopy3);
             return stateCopy3;
         default:
             return state;
