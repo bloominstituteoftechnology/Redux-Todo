f7f9aebafedc6031767ecbbc2ccbb8cb563de296
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2018-05-16 17:13:29 -0500

    Wrapped App component with Provider component to initalize pull request

---
+++ todo/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from  'react-redux';
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render(
+<Provider>
+<App />
+</Provider>,
+document.getElementById('root')
+);
