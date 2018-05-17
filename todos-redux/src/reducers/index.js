71bbd4aeb9a13b3d3ce228eb8d146e16b3af561b
Bee C <b.choeung@gmail.com>
2018-05-17 15:55:07 -0400

    completed most of the assignment but can't fix bug

---
+++ todos-redux/src/reducers/index.js
@@ -0,0 +1,21 @@
+import { ADD_TODO, COMPLETE_TODO } from '../actions';
+
+const initialState = [
+    {task: 'Learn Redux', completed: false, id: Date.now()}
+];
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case ADD_TODO:
+            return state.concat(action.payload);
+        case COMPLETE_TODO:
+            return state.map(todo => {
+                if (todo.id === action.payload) {
+                    return Object.assign({}, todo, {completed: !todo.completed});
+                } else {
+                    return todo;
+                }
+            });
+        default:
+            return state;
+    }
+};
