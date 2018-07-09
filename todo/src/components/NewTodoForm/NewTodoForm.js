fd84a537521013bad429db7050017f5616c6ba77
Kevin <brackkevin@gmail.com>
2018-07-09 17:18:44 -0500

    Added Todo component and put form elements in NewTodoForm

--- todo/src/components/NewTodoForm/NewTodoForm.js
+++ todo/src/components/NewTodoForm/NewTodoForm.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 
 export default class NewTodoForm extends Component {
   render() {
-    return <div>NEW TODO FORM</div>;
+    return (
+      <div>
+        <input type="text" name="newTodo" />
+        <button type="submit">SUBMIT</button>
+      </div>
+    );
   }
 }
