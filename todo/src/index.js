68cfc3aa095509f44a2e2a348c956b5c299f31e8
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-07-09 13:15:15 -0700

    initial commit

---
+++ todo/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(() => {})
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+registerServiceWorker();
