d4e87955d22e81bf497d73b3856ecd79f2465a35
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-07-09 14:06:52 -0500

    Ahmed Belhadj - Redux Todo
    Created store in index.js.

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers";
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
