39f9412856389b82f7c8c50a6af4d32ff6ce5df9
Simon <Simon_Freelance@hotmail.com>
2018-07-09 17:17:34 -0400

    Imported createStore and Provider and wrapped App component

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
