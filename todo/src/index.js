174da681c4a40c9774eddad590898509c5bfb5ef
Kevin <brackkevin@gmail.com>
2018-07-09 16:04:06 -0500

    src/index.js imported components and functions from redux, created and linked a reducers/index.js

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers";
+
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
