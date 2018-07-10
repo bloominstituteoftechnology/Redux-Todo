dd425722008d2cf83f25a5e93f6315e6689e3e61
Tyson Gushiken <tysonism@gmail.com>
2018-07-10 12:59:34 -0700

    Build todo state tree

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoReducer from './reducers/todoReducer';
 
 import App from './App';
 import './index.css';
 
+const store = createStore(
+  todoReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root')
+);
---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,14 @@
+const todos = {
+  todos: [
+    {value: 'To do 0', completed: false},
+    {value: 'To do 1', completed: false},
+    {value: 'To do 2', completed: false},
+    {value: 'To do 3', completed: false}
+  ]
+};
+
+const todoReducer = (state = todos, action) => {
+  return state;
+};
+
+export default todoReducer;
