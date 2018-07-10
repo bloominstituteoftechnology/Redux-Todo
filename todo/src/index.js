9a87b47e544cd9f0bc3ef2e431f0dc8b3c8af6f8
emaquino44 <prexy44@yahoo.com>
2018-07-10 16:07:35 -0700

    createstore and added provider to app.js

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { listReducer } from './reducers';
 
+const store = createStore(listReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+     <Provider store = {store}>
+     			<App />
+     			</Provider>,
+                document.getElementById('root'));
 
---
+++ todo/src/reducers/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+
+export const listReducer = (state =[], action => {
+	return state;
+})
+
