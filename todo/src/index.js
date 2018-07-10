c981ac51d386961260021b30964a67198d332b8e
Simon <Simon_Freelance@hotmail.com>
2018-07-10 16:20:30 -0400

    Setup the store for the app

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import reducer from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-
-ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
