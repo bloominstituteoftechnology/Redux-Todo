aecd81fc93e447857f191a7072c4d50cff19f59d
Alec <afroenygam@gmail.com>
2018-07-10 19:30:20 -0400

    Toggle functionality updated and working

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -16,12 +16,23 @@ export default (state = initialList, action) => {
       newTodos.push(action.payload);
       return Object.assign({}, state, {todos: newTodos});
     case TOGGLE_TODO:
-      return state.todos.map(todo => {
+      {/*return state.todos.map(todo => {
         if (todo.id === action.payload) {
           return Object.assign({}, todo, {completed: !todo.completed});
         }
         return todo;
-      });
+        });*/}
+
+        let toggleTodos = state.todos.slice().map(todo => {
+          if (todo.id === action.payload) {
+            todo.completed = !todo.completed;
+            return todo;
+          }
+          return todo;
+        })
+        return Object.assign({}, state, {todos: toggleTodos});
+      case DELETE_TODO:
+        return 'yes';
     default:
       return state;
   }
