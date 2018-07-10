84dab00f2db6a54216b8d0c38d5a702f8e3440a3
Simon <Simon_Freelance@hotmail.com>
2018-07-10 19:39:16 -0400

    Added more spaces to read it better

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_TODO, CHANGE_TODO, TOGGLE_COMPLETION } from "../actions";
+import { ADD_TODO, CHANGE_TODO, TOGGLE_COMPLETION, REMOVE_COMPLETED } from "../actions";
 
 
 const initialState = {
@@ -12,13 +12,21 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
+    console.log(action.type)
     switch(action.type) {
+        
         case ADD_TODO:
         return Object.assign({}, state, { todos: [...state.todos, action.payload]})
+        
         case CHANGE_TODO:
         return Object.assign({}, state, { text: action.payload })
+        
         case TOGGLE_COMPLETION:
         return Object.assign({}, state, { todos: action.payload });
+
+        case REMOVE_COMPLETED:
+        return Object.assign({}, state, { todos: action.payload});
+        
         default:
         return state;
     }
