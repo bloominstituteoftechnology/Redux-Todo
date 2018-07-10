8d79ad977a710c042c7e45a1c61cf696128ca90e
Kevin <brackkevin@gmail.com>
2018-07-10 16:24:12 -0500

    Added state with newTodoValue to NewTodoForm component. Also added handleChange method to update state based on input

--- todo/src/components/NewTodoForm/NewTodoForm.js
+++ todo/src/components/NewTodoForm/NewTodoForm.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 
 export default class NewTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodoValue: ""
+    };
+  }
+
+  handleChange = event => {
+    event.preventDefault();
+    this.setState({ newTodoValue: event.target.value });
+  };
+
   render() {
     return (
       <div>
         <form>
-          <input type="text" name="newTodo" placeholder="Add TODO..." />
+          <input
+            type="text"
+            name="newTodo"
+            placeholder="Add TODO..."
+            value={this.state.newTodoValue}
+            onChange={this.handleChange}
+          />
           <button type="submit">SUBMIT</button>
         </form>
       </div>
