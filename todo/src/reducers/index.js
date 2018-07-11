938e21193a79caa5d5eee94765f811714a5f628a
chainchompa <chainchompa@gmail.com>
2018-07-11 15:53:20 -0400

    added reducer

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,16 @@
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from "../actions";
+
+export const todos = (state = [], action) => {
+  switch(action.type) {
+    case ADD_TODO :
+      return state.concat([action.todo])
+    case REMOVE_TODO :
+      return state.filter((todo) => todo.id !== action.id)
+    case TOGGLE_TODO :
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, {complete: !todo.complete})
+      )
+    default :
+      return state
+  }
+}
