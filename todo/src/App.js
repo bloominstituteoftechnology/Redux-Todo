3b7e8fe98a1e6ae3ae5d4be788c2379024f95eea
TangledTessellations <mcevoya@oregonstate.edu>
2018-07-10 17:35:09 -0700

    Keep todos persist in local storage

--- todo/src/App.js
+++ todo/src/App.js
@@ -11,6 +11,13 @@ class App extends Component {
   handleNewTodo = (e) => {
     this.setState({newTodo: e.target.value});
   }
+
+  componentDidMount(){
+    if(localStorage.getItem("todo0")){
+      console.log(localStorage.getItem("todo0"));
+    }
+  }
+
   render() {
     return (
       <div className="App">
--- todo/src/reducers/todoReducer.js
+++ todo/src/reducers/todoReducer.js
@@ -1,26 +1,24 @@
 
 const initialState = {
-    todos: [{
-        todo: "Learn Javascript",
-        completed: false
-    }, 
-    {
-        todo: "Learn Redux",
-        completed: true
-    }, 
-    {
-        todo: "Profit",   
-        completed: false
-    }],
+    todos: localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [],
     newTodo: '',
 }
 const todoReducer = (state = initialState, action) => {
     switch(action.type) {
         case "ADD_TODO": 
-            const newTodos = state.todos.concat(action.payload);
-            return Object.assign({}, state, {todos: newTodos, newTodo: ""});
+            if(action.payload !== ""){
+                const newTodos = state.todos.concat(action.payload);
+                localStorage.setItem('todos', JSON.stringify(newTodos));
+                newTodos.map((todo,index) => localStorage.setItem(`todo${index}`, todo.todo));
+                return Object.assign({}, state, {todos: newTodos, newTodo: ""});
+            }
+            else{
+                return state;
+            }
         case "REMOVE_TODOS":
-            return Object.assign({}, state, {todos: state.todos.filter(todo => todo.completed === false)});
+            const newTodos = state.todos.filter(todo => todo.completed === false);
+            localStorage.setItem('todos', JSON.stringify(newTodos));
+            return Object.assign({}, state, {todos: newTodos});
         case "TOGGLE_COMPLETED":
             return Object.assign({}, state, {todos: state.todos.map(todo => {
                 if(todo.todo === action.payload){
