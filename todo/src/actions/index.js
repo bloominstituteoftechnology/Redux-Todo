bf12d362d41ccf8468d828740ad2c344d34854e6
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-07-13 12:17:24 +0100

    implemented Reducers

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,3 +1,8 @@
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+
 export const addTodo = text => ({
     type : 'ADD_TODO',
     id : Date.now(),
@@ -15,3 +20,9 @@ export const setVisibilityFilter = filter => ({
     type : 'SET_VISIBILITY_FILTER',
     filter
 })
+
+export const VisbilityFilters = {
+    SHOW_ALL : 'SHOW_ALL',
+    SHOW_COMPLETED : 'SHOW_COMPLETED',
+    SHOW_ACTIVE : 'SHOW_ACTIVE'
+}
