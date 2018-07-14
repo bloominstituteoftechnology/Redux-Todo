86ac1ab718e6ff13ed11e9f5e93fd7c6adb26871
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-07-14 08:31:37 +0100

    refactoring actions and reducer

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -2,27 +2,33 @@ export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 
+export const VisbilityFilters = {
+    SHOW_ALL : 'SHOW_ALL',
+    SHOW_COMPLETED : 'SHOW_COMPLETED',
+    SHOW_ACTIVE : 'SHOW_ACTIVE'
+}
 
+//initializing todo id
+const initId = 0
+
+//addTodo Action creator
 export const addTodo = text => ({
     type : 'ADD_TODO',
-    id : Date.now(),
+    id : initId++,
     text,
     completed : false
 })
 
+//toggleTodo action creator
 export const toggleTodo = id => ({
     type : 'TOGGLE_TODO',
     id,
 
 })
 
+//visibilility action creator
 export const setVisibilityFilter = filter => ({
     type : 'SET_VISIBILITY_FILTER',
     filter
 })
 
-export const VisbilityFilters = {
-    SHOW_ALL : 'SHOW_ALL',
-    SHOW_COMPLETED : 'SHOW_COMPLETED',
-    SHOW_ACTIVE : 'SHOW_ACTIVE'
-}
--- todo/src/reducers/todoReducer.js
+++ todo/src/reducers/todoReducer.js
@@ -1,15 +1,17 @@
+import { ADD_TODO,TOGGLE_TODO } from '../actions/index';
+
+//implementing todoReducer
 const todos = (state = [], action) => {
     switch (action.type) {
-        case 'ADD_TODO':
+        case ADD_TODO:
             return [...state , {
                 id : action.id,
                 text : action.text,
                 completed : action.completed
             }]
-        case 'TOGGLE_TODO':
+        case TOGGLE_TODO:
             return state.map( todo =>
-                        ( todo.id === action.id) ? { ...todo, completed : !todo.completetd} : todo)
-
+                        ( todo.id === action.id ) ? { ...todo, completed : !todo.completetd} : todo )
         default:
             return state
     }
