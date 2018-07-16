bdad883b3c5b75360b34c13cfadddaebf7a3bc03
Katie Gorbell <katiegorbell@gmail.com>
2018-07-16 17:28:21 -0500

    Built out basic structure of app

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from './Components/Form';
+import TodoList from './Components/TodoList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <h1>Todo List:</h1>
+       <Form />
+       <TodoList />
       </div>
     );
   }
---
+++ todo/src/Components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Form = () => {
+    return ( 
+        <form>
+            <input
+                type='text'
+                placeholder='Type todo here!'
+            />
+            <button>Submit</button>
+        </form>
+     );
+}
+ 
+export default Form;
