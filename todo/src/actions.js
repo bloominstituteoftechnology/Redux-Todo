294cc72330069574efd19595d374af135aa4f261
David Layman <davidlayman85@gmail.com>
2018-07-17 16:23:15 -0400

    David Layman React-redux

---
+++ todo/src/actions.js
@@ -0,0 +1,7 @@
+const submitOrDie = () => {
+	return {
+	type: "SUBMIT"
+	};
+};
+
+export default submitOrDie;
---
+++ todo/src/reducer.js
@@ -0,0 +1,27 @@
+const initial = {
+
+todos:[
+
+{
+  value: 'Walk the dog.',
+  completed: false
+},
+{
+value: 'Learn how to code.',
+completed: false
+}
+
+	]
+
+};
+
+const reducer = (state = initial, action) => {
+	switch(action.type){
+		case "SUBMIT":
+			return console.log('called!');
+		default:
+			return state;
+	}
+};
+
+export default reducer;
