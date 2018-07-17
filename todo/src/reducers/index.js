5aeba73dcda3992e24e56ece8ddde3be8fb508ae
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-07-17 15:51:39 -0600

    Todo Form

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -3,7 +3,6 @@ import { ADD_TODO, UPDATE_TODO, REMOVE_TODO } from '../actions';
 const initialState = [{ text: 'Clean Garage', completed: false, id: 0 }];
 
 export const todosReducer = (state = initialState, action) => {
-    console.log('Reducer Info', state, action.type, action.payload);
     switch (action.type) {
         case ADD_TODO:
         return Object.assign({}, state, {
@@ -16,5 +15,4 @@ export const todosReducer = (state = initialState, action) => {
     }
 };
 
-
-// export default todosReducer;
+ export default todosReducer;
