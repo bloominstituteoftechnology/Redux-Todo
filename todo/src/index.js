42e0a0f98497e1624f62cdd0191ccecd0991bd19
sean chavez <seanchavez@seanchavezsMBP.home>
2018-07-17 14:54:40 -0700

    Implement rootReducer; add Redux Devtools

--- todo/src/index.js
+++ todo/src/index.js
@@ -6,8 +6,13 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer'
 
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <Provider store={createStore(rootReducer)}>
+  <Provider store={store}>
     <App />
   </Provider>  
   , document.getElementById('root')
--- todo/src/reducers/rootReducer.js
+++ todo/src/reducers/rootReducer.js
@@ -1,5 +1,16 @@
-const rootReducer = () => {
-  console.log('nooooo...');
+const rootReducer = (todos = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...todos, action.payload];
+    case TOGGLE_TODO:
+      return todos.map(todo => {
+        if (todo.id === action.payload) {
+          return Object.assign({}, todo, { completed: !todo.completed });
+        }
+        return todo;
+      });
+  }
+  return todos;
 }
 
 export default rootReducer;
