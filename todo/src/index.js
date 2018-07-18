aab1554fd6045a8827a3be83c05eb45df88c772b
cubsfan46350 <cubsfan.46350@msn.com>
2018-07-18 00:18:53 -0500

    boilerplated app

---
+++ todo/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+
+const store = createStore(App);
+
+
+
+ReactDOM.render(
+    <Provider store={store}>><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
