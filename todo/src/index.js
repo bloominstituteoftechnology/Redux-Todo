04f89a70def266f7f48b1d64c7f02ae58ab07afe
nickolaussmith <nickolaussmith721@gmail.com>
2018-07-18 09:02:49 -0600

    MVP completed, add todo and toggle todo.

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {Provider} from 'react-redux';
+
+
+
+import {createStore} from 'redux';
+import todoReducer from './reducers/todoReducer';
+
+const store = createStore(todoReducer);
+
+
+
+ReactDOM.render(
+<Provider store={store}>
+<App /> 
+</Provider>,
+document.getElementById('root'));
