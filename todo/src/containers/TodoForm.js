35d43da2a6837efb8bedd3c991d8b46e025dd76b
sean chavez <seanchavez@seanchavezsMBP.home>
2018-07-17 20:45:59 -0700

    Add handlers for text input and addTodo button; addTodo not working

--- todo/src/containers/TodoForm.js
+++ todo/src/containers/TodoForm.js
@@ -3,12 +3,25 @@ import { addTodo, toggleTodo } from '../actions/todoActions';
 import { connect } from 'react-redux';
 
 class TodoForm extends React.Component {
-  state = { todo: '' }
+  state = { todoText: '' }
+
+  inputHandler = (e) => {
+    this.setState({ todoText: e.target.value });
+  }
+  addHandler = (e) => {
+    const todo = {
+      value: this.state,
+      completed: false,
+      id: Math.random()
+    }
+    this.props.addTodo(todo);
+    this.setState({ toDotext: '' })
+  }
   render() { 
     return (
       <div>
-        <input type="text"/>
-        <button>Add Todo</button>
+        <input onChange={this.inputHandler} type="text"/>
+        <button onClick={this.addHandler}>Add Todo</button>
       </div>
       );
   }
