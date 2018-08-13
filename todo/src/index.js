fc8fcf7045996cbbc52965b461c507e453c8cada
Lauren <llwor94@gmail.com>
2018-08-13 15:20:30 -0700

    data persists in local storage

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware} from 'redux';
-import { Provider } from 'react-redux';
-import logger from 'redux-logger';
-import App from './App';
-import todos from './reducers'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import App from "./App";
+import todos from "./reducers";
 
-const store = createStore(todos, applyMiddleware(logger))
+const persistedState = localStorage.getItem("todos")
+  ? JSON.parse(localStorage.getItem("todos"))
+  : [];
+
+const store = createStore(todos, persistedState, applyMiddleware(logger));
+
+store.subscribe(()=>{
+  localStorage.setItem('todos', JSON.stringify(store.getState()))
+})
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>, 
-document.getElementById('root'));
+  </Provider>,
+  document.getElementById("root")
+);
