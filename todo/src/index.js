3bb873058b3eed22e19451dd31b670c3c689e1f9
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-08-13 16:32:00 -0400

    Added redux and react-redux
    -yarn install & import in index.js

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>, document.getElementById('root'));
