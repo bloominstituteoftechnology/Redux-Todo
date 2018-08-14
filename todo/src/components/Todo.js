9c61cfa481cb96f850a3c22b963ea77d468e1591
Erik Kimsey <erik@erikkimsey.com>
2018-08-14 18:46:49 -0400

    I deleted the entire proejct I had started earlier and started over. Redux (rather, its react-redux counterpart) has been a monster.

--- todo/src/components/Todo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-
-export const Todo = (props) => {
-
-
-    return <div>TODO</div>
-}
--- todo/src/index.js
+++ todo/src/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import styled from 'styled-components';
+import registerServiceWorker from './registerServiceWorker';
 
-
-ReactDOM.render(<Provider store={createStore(() => { },
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)}>
-    <App />
-</Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
