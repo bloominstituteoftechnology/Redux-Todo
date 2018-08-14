86a71aaa159f976d1e544c9dcf76b742850f9188
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-08-14 15:48:41 -0500

    got project started and began creating store

---
+++ todo/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
+registerServiceWorker();
