307350a169bb4de025926d3c30d1cd3120927a6f
Trevor <trevorfehrman@gmail.com>
2018-08-14 13:35:55 -0700

    Cleanup

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import reducer from "./Reducers/index";
-// import { loadState, saveState } from "./localStorage";
 
-// const persistedState = loadState();
 
 const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : []
 
@@ -17,9 +15,7 @@ const store = createStore(
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-// store.subscribe(() => {
-// 	saveState(store.getState());
-// });
+
 
 store.subscribe(()=>{
 	localStorage.setItem('reduxState', JSON.stringify(store.getState()))
