d4dfc29c25331dd5b9b130ad63ea9abb0f11958b
codejoncode <jonathanjholloway@gmail.com>
2018-08-14 13:49:50 -0500

    wrapped Provider around app in the index file also created folders for the groups reducers actions and components along with a index.js file for each.

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
+
