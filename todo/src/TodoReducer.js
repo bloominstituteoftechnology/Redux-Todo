82a1ff56fbdfc6195e9446ef3359c3ef748d7caf
Sean <sean47900@gmail.com>
2018-08-14 16:33:20 -0500

    Done with Actions, working on other stuff

---
+++ todo/src/TodoReducer.js
@@ -0,0 +1,12 @@
+import{NEW_TODO, TOGGLE_COMPLETE} from './Actions'; 
+ export default (todos = [], action) =>{
+    switch(action.type){
+        case NEW_TODO:
+        return todos.concat(action.payload);
+        case TOGGLE_COMPLETE:
+        todos [action.payload].complete = !todos[action.payload].complete;
+        return todos; 
+        default: 
+        return todos; 
+    }
+}
--- todo/src/index.js
+++ todo/src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './Reducers';
 
+const store = createStore(
+    rootReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); 
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>,
+ document.getElementById('root')
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
