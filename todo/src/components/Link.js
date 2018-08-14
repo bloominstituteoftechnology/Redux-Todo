9a465a7077c9341224b527ccda6fef83e311095c
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-08-14 15:53:42 -0400

    Finished MVP gonna add delete now

--- todo/src/components/Link.js
+++ todo/src/components/Link.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const = Link ({active, children, onClick}) => (
+const  Link = ({active, children, onClick}) => (
     <button
     onClick={onClick}
     disabled={active}
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom'
+import {Provider} from 'react-redux';
+import {createStore} from 'redux'
+import rootReducer from './reducers';
 import './index.css';
-import App from './App';
+import App from './components/App';
+
+const store = createStore(rootReducer)
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+<Provider store = {store}>
+<App />
+</Provider>,
+ document.getElementById('root')
+);
 
