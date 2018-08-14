7f906251acc825b7859927ac7c791fbc7f9c0242
Trevor <trevorfehrman@gmail.com>
2018-08-14 13:35:15 -0700

    Local sate persistence implemented

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Todos from "./Components/Todos";
 import "./App.css";
 
+
 class App extends Component {
 	render() {
 		return (
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import reducer from "./Reducers/index";
+// import { loadState, saveState } from "./localStorage";
 
-const store = createStore(reducer, [], window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// const persistedState = loadState();
+
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : []
+
+const store = createStore(
+	reducer,
+	persistedState,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// store.subscribe(() => {
+// 	saveState(store.getState());
+// });
+
+store.subscribe(()=>{
+	localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+  })
 
 ReactDOM.render(
 	<Provider store={store}>
@@ -14,4 +31,3 @@ ReactDOM.render(
 	</Provider>,
 	document.getElementById("root")
 );
-
