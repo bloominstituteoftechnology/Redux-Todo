2c90cc4a6d3dddee85a77b41dbe57bf4e4083555
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-08-14 15:18:57 -0500

    Working from the index
    finished the index.js where the providers are held now moving on to the Reducer cause that is where I am getting an error because nothing is finished there.

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,5 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    rootReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); 
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>,
+ document.getElementById('root')
+);
