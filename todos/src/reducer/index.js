3a15d65ef2b161e9624ccdc94fc055816fd1ea53
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-08-14 14:50:34 -0600

    Added action creators and added actions to reducer

--- todos/src/reducer/index.js
+++ todos/src/reducer/index.js
@@ -23,7 +23,22 @@ const initialState = {
     ]
 }
 export default (state = initialState, action) => {
-    return {
-
+    switch(action.type) {
+        case('ADD_TODO'):
+            return{
+                todos: [...state.todos, action.payload]
+            }
+        case('TOGGLE_TODO'):
+            let updatedToggles = state.todos.map((item) => {
+                if(item.id === action.payload){
+                    item.completed = !item.completed
+                    return item;
+                }else {
+                    return item; 
+                }
+            })
+            return {
+               todos: updatedToggles
+            }
     }
 }
