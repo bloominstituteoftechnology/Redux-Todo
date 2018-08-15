5511c213a5bcceac9008b58addf9f3c2d3daab6f
Kairi <kairijb@gmail.com>
2018-08-14 21:22:24 -0500

    created reducer

--- todo/src/reducers/reducers.js
+++ todo/src/reducers/reducers.js
@@ -1,9 +1,15 @@
-import {ADD_TODO, } from '../actions/actions'
+import {ADD_TODO} from '../actions/actions'
 
 
-const itnitialState = {
-    todo: [{
-        task: 'wash dog',
-        completed: false
-    }]
+
+export default (todos=[], action) => {
+    switch(action.type) {
+        case ADD_TODO:
+        return [...todos, {
+            todo: action.todo,
+            id: action.id
+        }]
+        default:
+        return todos
+    }
 }
