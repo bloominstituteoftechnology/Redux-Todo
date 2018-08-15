54510dc23d82c7c144e9def93a512665d091220f
Charlie Winslow <cwinslow22@yahoo.com>
2018-08-14 22:30:26 -0500

    partially built out TodoForm

---
+++ todo/src/components/todoForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addTodo } from '../actions';
+
+class TodoForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            text: ''
+        };
+    }
+
+    todoInput = e => {
+        this.setState({ [e.target.name]: e.target.value });
+      }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input placeholder='Enter new task ...' name='text' value={this.state.text} onChange={this.todoInput} />
+                    <button type='submit' >Let's Do It</button>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+const mapStateToProps = (state) => {
+    return {
+        todos: state.todos
+    };
+};
+
+export default connect(mapStateToProps, { addTodo })(TodoForm);
