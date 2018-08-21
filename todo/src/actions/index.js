e00b8a96cc405d996cce29904beb46846acd6792
farste <sfargali@gmail.com>
2018-08-21 18:52:12 -0400

    Stephen Fargali Set up file structure, started setting up actions

---
+++ todo/src/actions/index.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+    todo: [
+        
+    ],
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case :
+        return {
+            todo: st
+        }
+    }
+}
