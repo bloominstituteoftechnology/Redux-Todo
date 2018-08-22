a822e20376aea081042f89c5e1b67fd4546410e5
Synch3 <jonsimo@aol.com>
2018-08-22 09:06:07 -0400

    working on index

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import todos from './reducers';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+const store = createStore(
+  todos,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
