1cc1e6e2789ab1ab8a7b7e2e6058913768baa52a
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-08-22 10:26:22 -0500

    more setup

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+const store = createStore(todoReducer);
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
