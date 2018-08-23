4e48ba690e0fe67a2e40b5b97409a3c4151ccb7e
vanceleon <vanceleon44@gmail.com>
2018-08-23 01:05:47 -0400

    building out the App.js component

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,21 +1,66 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {connect} from 'react-redux';
+import TodoList from './Components/TodoList';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      newTodo: "",
+
+    }
+  }
+  
+  // updateInputValue = event => {
+    
+    
+  // }
+
+  submitNewTask = event => {
+    event.preventDefault();
+    this.props.addTodo({
+      value: this.state.newTodo,
+      compelet: false,
+    });
+    this.setState({
+      newTodo: ''
+    });
+  }
+
+  newTodoChange = (event) =>{
+    this.setState({
+      newTodo: event.target.value
+    });
+  }
+
+
+  
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit="newTodoAdder">
+            <label>Enter New Todo Here</label>
+            <input
+              onchange={this.newTodoChange}
+              placeholder="New Todo"
+              value={this.state.newTodo}/>
+
+        </form>
+        <TodoList todos={this.props.todos}/>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return(
+    <div>connection</div>
+    // todos: state.todos
+  )
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- todo/src/index.js
+++ todo/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import reducers from "./reducers";
+import rootReducer from "./reducers";
 
 import "./index.css";
 import App from "./App";
