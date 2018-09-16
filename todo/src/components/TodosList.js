83d67f295c372c1ae2ac7b43fb4ce2e2eff17f4c
marshall lanners <marshalllanners@marshalls-MacBook-Air.local>
2018-09-15 21:50:50 -0700

    finished mvp todos react

---
+++ todo/src/components/TodosList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { connect } from "react-redux";
+import Todo from './Todo';
+import { toggleTodo} from '../actions';
+
+const TodosList = ({todos, toggleTodo, deleteTodo}) => {
+	console.log(todos);
+	return (
+		<ul>
+		  {todos.map((todo) =>
+		  	<Todo key={todo.id}
+		  		{...todo}
+		  		onClick={() => toggleTodo(todo.id)}
+
+		  	/>
+		  )}
+		</ul>
+	)
+}
+
+const mapStateToProps = state => {
+  return {
+    todos: state,
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+	toggleTodo: id => dispatch(toggleTodo(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodosList);
