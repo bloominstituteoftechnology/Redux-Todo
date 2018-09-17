4810cc50ca556e0478e447ff51399eb97b87bf18
Timothy Hoang <timh1203@gmail.com>
2018-09-17 16:45:32 -0400

    Add todos component

---
+++ todo/src/components/Todos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <div>
+      <h2>Todos Component</h2>
+    </div>
+  )
+}
+
+export default Todos
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,3 +1,11 @@
-export default function (state = {}, action) {
-  return state
+
+const initialState = {
+  todos: []
+}
+
+export default function (state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state
+  }
 }
