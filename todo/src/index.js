ff804149beead147515d13ceffc289f464710d5d
decagondev <decagondevelopment@gmail.com>
2018-09-17 22:47:20 +0100

    (MVP) : started adding redux boilerplate to the index.js file

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = () => {};
+
+const store = createStore(reducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
