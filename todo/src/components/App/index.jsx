aa783c72494a7c01612734ccc2671b926c5696e2
decagondev <decagondevelopment@gmail.com>
2018-09-17 22:41:27 +0100

    (MVP) : used create react app to initialize the app and doing a little housekeeping

---
+++ todo/src/components/App/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        Hello Redux
+      </div>
+    );
+  }
+}
+
+export default App;
