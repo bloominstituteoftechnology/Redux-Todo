cd47b07f5e5da04a2bb8b303b7f3d08ee39dc012
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-09-17 18:21:16 -0400

    Boilerplate

---
+++ todo/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(<App />, document.getElementById('root'));
