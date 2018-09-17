d835566d8edfe0d111f0bdafe50ee8327c46e23b
decagondev <decagondevelopment@gmail.com>
2018-09-17 23:04:10 +0100

    (MVP) : added some structure to the action creators

---
+++ todo/src/actions/index.js
@@ -0,0 +1,7 @@
+// TODO:: create some action creators
+
+// input handler action takes in an input as a payload returns a HANDLE_INPUT action with a payload of input
+export const handleInput = input => {}
+
+// add todo action returns a ADD_TODO action
+export const addTodo = () => {}
