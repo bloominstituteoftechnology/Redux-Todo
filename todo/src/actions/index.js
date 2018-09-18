a085f403566eac9bbb80615b4d5861ac1fe7aade
MikeRGarrity <mikergarrity@gmail.com>
2018-09-18 16:07:18 -0500

    adding reducer function, action creators, and adding actions to reducer

---
+++ todo/src/actions/index.js
@@ -0,0 +1,12 @@
+export const addTodo = (newTodo) => {
+  return {
+      type: "ADD_TODO", 
+      payload: newTodo
+  }
+}
+export const toggleTodo = (id) => {
+  return {
+      type: "TOGGLE_TODO", 
+      payload: id
+  }
+} 
--- todo/src/index.js
+++ todo/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'; 
+import { Provider } from 'react-redux';
+import todoReducer from './reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  todoReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, 
+    document.getElementById('root'));
 registerServiceWorker();
