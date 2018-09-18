d6128cabbd3f5e0a8eab08f7bce03fc39dc8b4ad
unknown <cornielleandres@gmail.com>
2018-09-18 17:54:11 -0400

    Add fade anim to app

--- todo/src/App.js
+++ todo/src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 class App extends Component {
 	render() {
 		return (
-			<div className = 'app'>
+			<div className = 'app fade-in'>
 				<header className = 'app-header'>
 					<h1>Welcome to your Todo List</h1>
 				</header>
