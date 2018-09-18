22b6954433f4cac8478bc825983bba8f6e7b8f16
Michael Agard <mdagard@gmail.com>
2018-09-18 16:53:45 -0400

    start the project over

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createStore } from 'redux';
-// import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
 import App from './App';
 import './index.css';
 
-// const store = createStore(todo);
 ReactDOM.render(
-  <Provider
-    store={createStore(function() {
-      return {};
-    })}
-  >
+  <Provider store={createStore(reducers)}>
     <App />
   </Provider>,
   document.getElementById('root')
