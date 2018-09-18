e4ad48596f7f3f7fbd6fbb4ef2ef91fdc7039301
Michael Agard <mdagard@gmail.com>
2018-09-18 19:07:07 -0400

    modified cases

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,8 +1,10 @@
+import { ADD_TODO } from '../actions';
+
 export default function reducer(state, action) {
   console.log('in reducer: ', action);
   switch (action.type) {
-    case 'CHANGE_NUMBER':
-      return { ...state, number: action.payload };
+    case ADD_TODO:
+      return state.concat(action.payload);
     default:
       return state;
   }
