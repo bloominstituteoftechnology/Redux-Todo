9c3e42a2a80ccb2fbcc76685968fa7bae7b9e150
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-09-18 19:29:38 -0400

    Toggle line through task complete

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Todo = (props) => {
   return (
     console.log(props),
-    <li onClick={() => props.toggleComplete(props.todo)}>{props.todo.value}</li>
+    (
+      <li
+        onClick={() => props.toggleComplete(props.todo)}
+        style={{
+          textDecoration: props.todo.completed ? 'line-through' : 'none',
+        }}
+      >
+        {props.todo.value}
+      </li>
+    )
   );
 };
 
