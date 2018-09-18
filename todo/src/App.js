fe5eff337d296fe07610c15f06df3b77200a1f9c
Andrew <ghobrial.andrew@gmail.com>
2018-09-18 17:44:38 -0400

    app.js and actions index.js complete

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import { addTask } from './actions';
+import TodoList from './components/TodoList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTask: ''
+    };
+    this.addTask = this.addTask.bind(this);
+    this.handleNewTask = this.handleNewTask.bind(this);
+  }
+
+  addTask(event) {
+    event.preventDefault();
+    this.props.addTask({
+      value: this.state.newTask,
+      complete: false
+    });
+    this.setState({
+      newTask: ''
+    });
+  }
+
+  handleNewTask(event) {
+    this.setState({
+      newTask: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.addTask}>
+          <input
+            onChange={this.handleNewTask}
+            placeholder="new task"
+            value={this.state.newTask}
+          />
+        </form>
+        <TodoList tasks={this.props.tasks} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    tasks: state.tasks
+  };
+};
+
+export default connect(mapStateToProps, { addTask })(App);
---
+++ todo/src/actions/index.js
@@ -0,0 +1,16 @@
+export const NEW_TASK = 'NEW_TASK';
+export const TOGGLE_COMPLETE = 'TOGGLE_COMPLETE';
+
+export const addTask = (task) => {
+  return {
+    type: NEW_TASK,
+    payload: task
+  };
+};
+
+export const toggleComplete = (index) => {
+  return {
+    type: TOGGLE_COMPLETE,
+    payload: index
+  };
+};
