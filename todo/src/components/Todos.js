0c2f8b1b3ed21d1285bfc61b5ae66d01dd85dbd6
Michael Agard <mdagard@gmail.com>
2018-09-18 19:06:42 -0400

    added map for todos

--- todo/src/components/Todos.js
+++ todo/src/components/Todos.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Todo from './Todo';
-export default class Todos extends Component {
+class Todos extends Component {
   render() {
-    return <Todo />;
+    return (
+      <ul>
+        {props.todos.map(todo => (
+          <Todo
+            value={todo.value}
+            key={todo.id}
+            id={todo.id}
+            completed={todo.completed}
+          />
+        ))}
+      </ul>
+    );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    todos: state
+  };
+};
+
+export default connect(
+  mapStateToProps
+  // { value }
+)(Todos);
