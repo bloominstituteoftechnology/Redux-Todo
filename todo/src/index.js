924a0847be896fe1ea9fcb5b3485311f2eaf0ccd
Michael Agard <mdagard@gmail.com>
2018-09-18 16:17:30 -0400

    Provider added

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import todo from './reducers';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-const store = createStore(todo);
+// const store = createStore(todo);
 ReactDOM.render(
-  <Provider store={store}>
-    <Todo />
+  <Provider
+    store={createStore(function() {
+      return {};
+    })}
+  >
+    <App />
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
