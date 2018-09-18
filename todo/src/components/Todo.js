30a3c77d2152d2df46ad6d9f0e0a1e7d7028fa34
Michael Agard <mdagard@gmail.com>
2018-09-18 19:06:28 -0400

    added some todo functionality

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
-export default class Todo extends Component {
+import { connect } from 'react-redux';
+class Todo extends Component {
   render() {
-    return <p>Todo List</p>;
+    return (
+      <li
+        style={props.todo.completed ? { textDecoration: 'line-through' } : null}
+        onClick={() => {
+          props.toggleCompleted(props.todo.id);
+        }}
+        id={props.todo.id}
+      >
+        {props.todo.value}
+      </li>
+    );
   }
 }
+const mapStateToProps = state => {
+  return {
+    ...props
+    todos: state
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { value }
+)(Todos);
