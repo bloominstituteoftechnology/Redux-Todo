f8d9b138df01519f1899601f4343ab9fdf1ec0e2
Michael Agard <mdagard@gmail.com>
2018-09-18 16:17:44 -0400

    added ADD_TODO aciton

---
+++ todo/src/actions/index.js
@@ -0,0 +1,13 @@
+export const ADD_TODO = 'ADD_TODO';
+// Our action creators will return
+// an action packet that our reducer will
+// receive. What does the action packet look like?
+// Note that the action creator is not at all
+// responsible for handling any of the actual
+// logic of updating the central Redux store. That
+// is left to the reducer(s).
+
+export const addTodo = num => {
+  console.log('in action: ', num);
+  return { type: 'ADD_TODO' };
+};
