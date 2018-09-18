81305ac1c748f02c9a36bc6fa35e6772007c4c1f
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-09-18 16:19:00 -0400

    set up base index.js with store and provider information

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import todoReducer from './reducers';
 
-{/* first commit */}
+const store = createStore(todoReducer);
+
+ReactDOM.render(
+  <Provider store = {store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
