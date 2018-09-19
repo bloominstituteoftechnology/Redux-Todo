1089313d732ebc987c7005960fa7d1e704d2f79b
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-09-19 08:34:30 -0500

    mvp functional

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, 
+document.getElementById('root')
+);
