dbc3be331370e79efec95c8b88b84e611086c7b4
Timothy Hoang <timh1203@gmail.com>
2018-09-18 20:13:18 -0400

    Add completed task with class toggling

--- todo/src/components/Todos.js
+++ todo/src/components/Todos.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Todos.css'
 
 const Todos = (props) => {
   const items = props.todos.map(item => item)
@@ -10,9 +11,10 @@ const Todos = (props) => {
           items.map((todo)  =>{
             return (
               <li
+                class={`todo-item ${todo.completed ? 'lineThrough' : ''}`}
                 key={`todo-${todo.id}`}
                 onClick={() => props.toggleTodo(todo.id)}
-                style={{textDecoration: todo.completed ? 'line-through' : 'none'}}
+                // style={{textDecoration: todo.completed ? 'line-through' : 'none'}}
               >
                 {todo.value}
                 <button
