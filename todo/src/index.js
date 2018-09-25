94cfcc8ed5b866bae39dcdac8d70b2f7e1034c03
Joseph Chretien-Golden <joechre@gmail.com>
2018-09-25 17:59:08 -0500

    files components

--- todo/src/index.js
+++ todo/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from "react-redux";
+import { createStore } from 'redux';
 import todoReducer from './reducer'; 
 
 const store = createStore(todoReducer);
