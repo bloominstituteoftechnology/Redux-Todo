4718519bb47e8ba675bac415b14de7ff8f0772c2
lyang9 <lyang9@buffalo.edu>
2018-09-25 17:43:53 -0400

    Completed reducers

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,29 @@
+import { ADD_TODO, COMPLETE_TODO } from '../actions';
+
+const initialState = [{
+  id: 0,
+  value: 'Walk the dog',
+  complete: false
+}];
+
+export const todosReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case ADD_TODO:
+      return [...state, action.payload];
+    case COMPLETE_TODO:
+      let completeTodo = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          todo.completed = !todo.completed
+          return todo;
+        }
+        else {
+          return todo;
+        }
+      })
+      return {
+        todos: completeTodo
+      }
+    default:
+      return state;
+  };
+}
