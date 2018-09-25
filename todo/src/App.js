fcff084253bc7f4ee581bbded7aa603fb0e809a0
lyang9 <lyang9@buffalo.edu>
2018-09-25 19:18:22 -0400

    Imported TodoForm and created handleInputChange

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import Todos from './components/Todos';
+import TodoForm from './components/TodoForm';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todoText: ''
+      inputText: ''
     }
   }
+
+  handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+  
+  handleAddTodoSubmit = event => {
+    event.preventDefault();
+    
+  }
+
   render() {
     return (
       <div className="App">
+        <h1>Todo List</h1>
         <Todos todos={this.props.todos} />
+        <TodoForm 
+          inputText={this.state.inputText}
+          handleInput={this.handleInputChange}
+        />
       </div>
     );
   }
