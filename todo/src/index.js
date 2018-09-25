85aa527705c96bc002bc55b6eb1db446afc78b20
lyang9 <lyang9@buffalo.edu>
2018-09-25 17:42:18 -0400

    Added curly braces for todosReducer import

--- todo/src/index.js
+++ todo/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import todosReducer from './reducers';
+import { todosReducer } from './reducers';
 
 const store = createStore(todosReducer);
 
