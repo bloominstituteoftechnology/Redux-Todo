6d1f87efb721776836ec8bb40f227d9181374a53
lyang9 <lyang9@buffalo.edu>
2018-09-25 18:44:30 -0400

    Imported connect and Todos

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import Todos from './components/Todos';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoText: ''
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Todos todos={this.props.todos} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  };
+}
+
+export default connect(mapStateToProps)(App);
