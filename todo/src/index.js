13d62a20a36b61c661eafcd27c6396fa0b19c41a
Brandon White <bwhite06@stac.edu>
2018-09-25 15:07:10 -0400

    intial commit: working on creating a state

---
+++ todo/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {Provider} from 'react-redux';
+import {todoReducer} from './reducer/todoReducer.js';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(
+todoReducer,
+ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+  <App />
+</Provider>
+  , document.getElementById('root'));
+registerServiceWorker();
---
+++ todo/src/reducer/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+import { todoReducer } from './todoReducer';
+
+
+export default combineReducers({
+  todoReducer,
+});
