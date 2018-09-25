db729dca4906b5c08da051eb18872efd9977abb8
Matthew Wright <matthewbwright@gmail.com>
2018-09-25 15:16:00 -0700

    adds hover effect

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -9,6 +9,7 @@ function Todo(props) {
       {props.todos.map(todo => {
         return (
           <li
+            className="hvr-grow"
             key={todo.id}
             onClick={() => props.toggleCompleted(todo.id)}
             style={todo.completed ? { color: 'red' } : { color: 'black' }}
