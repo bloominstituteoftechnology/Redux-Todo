d318131cad61088a2074885d372661b2af7c6ce5
lyang9 <lyang9@buffalo.edu>
2018-09-26 15:47:02 -0400

    Used index on map and passed in props completeTodo from actions

--- todo/src/components/Todos.js
+++ todo/src/components/Todos.js
@@ -4,7 +4,14 @@ import { connect } from 'react-redux';
 const Todos = props => {
   return (
     <div>
-      {props.todos.map(todo => <li key={todo.id}>{todo.value}</li>)}
+      {props.todos.map((todo, index)=> (
+        <li 
+          onClick={() => props.completeTodo(index)}
+          key={todo.id}
+        >
+          {todo.value}
+        </li>
+      ))}
     </div>
   );
 }
