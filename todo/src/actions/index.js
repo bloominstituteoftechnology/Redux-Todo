212c24204cbbc938e76271fe34ce9f722d18b3ec
lyang9 <lyang9@buffalo.edu>
2018-09-26 15:42:13 -0400

    Changed payload from id to index

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -5,6 +5,6 @@ export const addTodo = todo => {
   return { type: ADD_TODO, payload: todo };
 };
 
-export const completeTodo = id => {
-  return { type: COMPLETE_TODO, payload: id };
+export const completeTodo = index => {
+  return { type: COMPLETE_TODO, payload: index };
 };
