96b597de13165dcf7f05db1e73865e746ed963f6
lyang9 <lyang9@buffalo.edu>
2018-09-27 00:03:40 -0400

    Added styling to completed todo

--- todo/src/components/Todos.js
+++ todo/src/components/Todos.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { completeTodo } from '../actions';
 
+const style = { textDecoration: 'line-through'};
+
 const Todos = props => {
   return (
     <div>
       {props.todos.map((todo, index)=> (
         <li 
+          style={todo.completed ? style : null}
           onClick={() => props.completeTodo(index)}
           key={todo.id}
         >
