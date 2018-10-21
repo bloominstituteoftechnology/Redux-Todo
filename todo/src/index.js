5fff46a66eff2c07e91535abbbc81072759fb198
Lee Len Formento <leebatef@gmail.com>
2018-10-21 16:42:15 -0400

    todoReducers and Redux test

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 
-const store = createStore();
+import { createStore } from 'redux'; // Redux store creation method
+import { Provider } from 'react-redux'; // Wrap around App component. Access to redux global store.
+
+// creating store by passing information (reducer: representation of state) to createStore. 
+// Testing with Redux Chrome extension
+const store = createStore(todosReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ); 
 
 ReactDOM.render(
 <Provider store={store}>
