be75123a87dc84fd46b2ef391b5925f641f761c7
SavannahGreuel <smallidgesavannah@gmail.com>
2018-10-22 16:23:43 -0600

    added reducer

---
+++ todo/src/Components/TodoList/todo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TodoList extends React.Component {
+    render() {
+        return(
+            <form>
+                <input type ='text' />
+                <input type = 'submit'/>
+            </form>
+        )
+    }
+}
+
+export default TodoList;
---
+++ todo/src/Components/reducer/index.js
@@ -0,0 +1,11 @@
+
+const initialState = {
+    todoArray: []
+};
+
+export default (state = initialState, action => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+})
