466c7f2958d74e6d07c5a5756dd86fb81b57111b
BrandonVilaychith <blvilaychith@gmail.com>
2018-10-22 16:53:07 -0500

    reducer created

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+import todoReducer from './reducers';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(() => {},
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+  todoReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ todo/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+  todos: []
+};
+
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
