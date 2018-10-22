e1b9b02bfa67ec04add5a0c6048c431c0c1899ca
TRIF3X <thewizardofcrypto@gmail.com>
2018-10-22 16:26:39 -0500

    made react redux todo using redux docs, created new todo for myself.

---
+++ redux-todo/src/components/Link.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Link = ({ active, children, onClick }) => (
+  <button
+     onClick={onClick}
+     disabled={active}
+     style={{
+         marginLeft: '4px',
+     }}
+  >
+    {children}
+  </button>
+)
+
+
+export default Link
