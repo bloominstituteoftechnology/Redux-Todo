335214d4687240fc79cfda2e2bed5d8c285bfd64
BrandonVilaychith <blvilaychith@gmail.com>
2018-10-22 17:30:58 -0500

    Added in todoform and list

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <h1>Todo List</h1>
+        <TodoForm />
+        <TodoList />
       </div>
     );
   }
