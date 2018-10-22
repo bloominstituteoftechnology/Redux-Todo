e47e2177eb9d875de8c3db91ea2ac32d3a9422da
BrandonVilaychith <blvilaychith@gmail.com>
2018-10-22 17:34:47 -0500

    Form functionality added

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,11 +1,26 @@
-import React from 'react';
-function TodoForm(props) {
-  return (
-    <form>
-      <input type="text" name="newTodo" />
-      <button>Add</button>
-    </form>
-  );
+import React, { Component } from 'react';
+class TodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodo: ''
+    };
+  }
+
+  changeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <form>
+        <input type="text" name="newTodo" onChange={this.changeHandler} />
+        <button>Add</button>
+      </form>
+    );
+  }
 }
 
 export default TodoForm;
