61158d94cd49a7fda81fab6ab455fc8187984425
Matthew Wright <matthewbwright@gmail.com>
2018-10-22 15:06:05 -0700

    sets up reducers

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,32 @@
+import {} from '../actions';
+
+export const initialState = {
+  todos: []
+};
+
+export function todoReducers(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        todos: [
+          ...state.todos,
+          { id: action.id, value: action.text, completed: false }
+        ]
+      };
+
+    case TOGGLE_COMPLETED:
+      const newTodos = state.todos.map(todo => {
+        if (todo.id === action.paylaod) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        } else {
+          return { ...todo };
+        }
+      });
+      return { todos: newTodos };
+    default:
+      return state;
+  }
+}
