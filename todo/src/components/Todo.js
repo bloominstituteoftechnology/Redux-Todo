72b77ee29d3cfed4d55d3a871ad003e35d347b40
unknown <brandongrych@gmail.com>
2018-10-23 16:15:38 -0500

    delete and add functions added

---
+++ todo/src/components/Todo.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { toggleComplete, deleteTodo } from "../actions";
+
+const Todo = props => {
+  return (
+    <div>
+      <p
+        onClick={() =>
+          toggleComplete(props.todo.value, props.todo.completed, props.id)
+        }
+      >
+        {props.todo.value}{" "}
+        <i
+          className="fas fa-trash-alt"
+          onClick={e => props.deleteTodo(e, props.id)}
+        />
+      </p>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  { toggleComplete, deleteTodo }
+)(Todo);
