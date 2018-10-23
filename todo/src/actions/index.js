a05540777c3f2a4483346dc6dae8bbebbb582dc9
Stublink <chrisrey95@gmail.com>
2018-10-23 15:42:43 -0400

    Action creators for reducer cases

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,5 +1,32 @@
 export const CREATE_TODO = 'create_todo';
+export const COMPLETE_TOGGLE = 'complete_toggle';
 export const UPDATE_TODO = 'update_todo';
 export const DELETE_TODO = 'delete_todo';
-export const COMPLETE_TOGGLE = 'complete_toggle';
 
+export const createTodo = (value) => {
+    return {
+        type: CREATE_TODO,
+        action: value
+    }
+};
+
+export const completeToggle = (index) => {
+    return {
+        type: COMPLETE_TOGGLE,
+        action: index
+    }
+};
+
+export const deleteTodo = (index) => {
+    return {
+        type: DELETE_TODO,
+        action: index
+    }
+};
+
+export const updateTodo = (index, value) => {
+    return {
+        type: UPDATE_TODO,
+        action: {index, value}
+    }
+};
