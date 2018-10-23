2e8a04cc2c4b21d6f0c86b5a01322784148d4041
kyranmccann <kyran.mccann@gmail.com>
2018-10-23 12:53:19 -0700

    enabled redux devtools

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import state from './reducers';
+import reducer from './reducers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(state)
+ const store = createStore(
+   reducer,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
