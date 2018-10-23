f1b6556f015ab6ee1740f02b382d4db9fef28f02
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-23 17:24:12 -0500

    Updates to reducers, container, and app js

--- todo/src/containers/TodoContainer.js
+++ todo/src/containers/TodoContainer.js
@@ -1,80 +1,81 @@
 import React from 'react';
-import { ADD_TODO, TOGGLE_TODO, GET_TODOS } from '../actions';
+import { addTodo, toggleTodo, getTodos } from '../actions';
 import { connect } from 'react-redux';
 
 class TodoContainer extends React.Component {
     constructor() {
-        super();
-        this.state = {
-            inputText: ''
-        };
+      super();
+      this.state = {
+        text: ''
+      };
     }
-
+  
     handleTodoComplete = todoId => {
-        this.props.toggleTodo(todoId);
+      this.props.toggleTodo(todoId);
+    };
+  
+    handleTodoInput = e => {
+      this.setState({ [e.target.name]: e.target.value });
+    };
+  
+    addTodoHandler = e => {
+      e.preventDefault();
+      const { text } = this.state;
+      const newTodo = {
+        text,
+        completed: false,
+        id: this.props.todos ? this.props.todos.length + text : 0 + text
       };
-    
-      handleTodoInput = e => {
-        this.setState({ [e.target.name]: e.target.value });
-      };
-    
-      addTodoHandler = e => {
-        const { text } = this.state;
-        const newTodo = {
-          text,
-          completed: false,
-          id: this.props.todos ? this.props.todos.length + text : 0 + text
-        };
-        this.props.addTodo(newTodo);
-        this.setState({
-          inputText: ''
-        });
-      };
-
-      render() {
-    const { todos } = this.props;
-    return (
-      <div>
-        <form onSubmit={this.addTodoHandler}>
-          <input
-            onChange={this.handleTodoInput}
-            name="text"
-            value={this.state.text}
-          />
-          <button type="submit" >
-            Add Todo
-          </button>
-        </form>
-        <ul>
-          {todos.map(todo => {
-            return (
-              <li
-                onClick={() => this.handleTodoComplete(todo.id)}
-                style={
-                  todo.completed
-                    ? { color: '#d3d3d3', textDecoration: 'line-through' }
-                    : null
-                }
-                key={todo.id}
-              >
-                {todo.inputText}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
+      this.props.addTodo(newTodo);
+      this.setState({
+        text: ''
+      });
+    };
+  
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.addTodoHandler}>
+            <input
+              onChange={this.handleTodoInput}
+              name="text"
+              value={this.state.text}
+            />
+            <button type="submit" >
+              Add Todo
+            </button>
+          </form>
+          <ul>
+            {this.props.todos.map(todo => {
+              return (
+                <ul
+                  onClick={() => this.handleTodoComplete(todo.id)}
+                  style={
+                    todo.completed
+                      ? { color: '#d3d3d3', textDecoration: 'line-through' }
+                      : null
+                  }
+                  key={todo.id}
+                >
+                  {todo.text}
+                </ul>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    }
   }
-}
-
-const mapStateToProps = state => {
-  return {
-    todos: state.todos
+  // Hey Redux?! Whatever is state in the store, could throw it on Props inside
+  // this react component?
+  const mapStateToProps = state => {
+    return {
+      todos: state.todos
+    };
   };
-};
-
-export default connect(mapStateToProps, {
-  addTodo,
-  toggleTodo,
-  getTodos
-})(TodoContainer);
+  
+  export default connect(mapStateToProps, {
+    addTodo,
+    toggleTodo,
+    getTodos
+  })(TodoContainer);
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -4,25 +4,25 @@ const initialState = {
     todos: []
   };
 
-const reducers = (state = initialState, action) => {
+  const reducers = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_TODO:
-          return {
-              ...state,
-              todos: [...state.todos, action.payload]
-          };
-        case TOGGLE_TODO:
-          return state.todos.map(todo => {
-            if (todo.id === action.payload) {
-              return Object.assign({}, todo, { completed: !todo.completed });
-            }
-            return todo;
-          });
-        case GET_TODOS:
-          return action.payload;
-        default:
-          return state;
-      }
-}
+      case ADD_TODO:
+        return {
+            ...state,
+            todos: [...state.todos, action.payload]
+        };
+      case TOGGLE_TODO:
+        return state.todos.map(todo => {
+          if (todo.id === action.payload) {
+            return Object.assign({}, todo, { completed: !todo.completed });
+          }
+          return todo;
+        });
+      case GET_TODOS:
+        return action.payload;
+      default:
+        return state;
+    }
+  };
 
 export default reducers;
