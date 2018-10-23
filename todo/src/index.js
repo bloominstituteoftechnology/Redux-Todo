c64b679ce5157698a25ab2f84c5daf338643b30b
sean-one <basement365@gmail.com>
2018-10-22 23:36:13 -0700

    added some of redux & react-redux

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore((state, action) => {});
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
