4d80a3d2d0ee84b7a1d479d95f09c169888ea3de
lola <lev@lolas-air.lan>
2018-10-23 15:41:11 -0400

    Switched App from class to functional

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
 
-class App extends Component {
-  	constructor(props) {
-		super(props);
-	}
-  	render() {
-		return (
-			<div className="App">
-				<h1>todo</h1>
-				<TodoForm />
-				<TodoList />
-			</div>
-		);
-  	}
-}
+const App = () => (
+	<div className="App">
+		<h1>todo</h1>
+		<TodoForm />
+		<TodoList />
+	</div>
+)
 
 export default App;
