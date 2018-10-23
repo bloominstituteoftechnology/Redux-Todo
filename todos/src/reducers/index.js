e2326485259d2d73a677260a65e930b5ad0e8339
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-23 15:55:54 -0700

    fix toggle completed bug

--- todos/src/reducers/index.js
+++ todos/src/reducers/index.js
@@ -1,25 +1,28 @@
 import {ADD_TODO, TOGGLE_COMPLETED} from "../actions";
 
-const initialState = {todos: [{value: "study", completed: false}]};
+const initialState = {todos: []};
 
 const todoReducer = (state = initialState, action) => {
   console.log("REDUCER", state, action);
   switch (action.type) {
     case ADD_TODO:
+      console.log("Original State: ", state);
       return {
         ...state,
         todos: [...state.todos, {value: action.payload, completed: false}]
       };
     case TOGGLE_COMPLETED:
       return {
-        ...state,
         todos: state.todos.map((todo, index) => {
+          console.log("Original State: ", state);
           if (index === action.payload) {
-            return {...todo, completed: !todo.completed};
+            todo.completed = !todo.completed;
           }
+          return todo;
         })
       };
     default:
+      console.log("Original State: ", state);
       return state;
   }
 };
