271aa634c162afb5d3bba4e85eba9907d21cdfe4
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-23 14:38:27 -0600

    implemented reducers

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,31 @@
+import { ADD_TODO } from '../actions';
+
+
+ const initialState = 
+{
+    todos: 
+    [
+        {
+            value: '',
+            completed: false
+        }
+    ]
+}
+ const todos = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TODO:
+            return {
+                ...state,
+                todos: [
+                    ...state.todos,
+                    {value: action.payload,
+                    completed: false
+                    }
+                ]
+            }
+            
+        default:
+            return state;
+    }
+}
+ export default todos; 
