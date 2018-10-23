9458e2da95fcae54fe6e3c90af6f439eee906d57
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-23 14:40:06 -0600

    created todolist.js to display todos

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AddTodo/>
+        <AddTodo />
+        <TodoList />
       </div>
     );
   }
---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+ const TodoList = ({ todos }) => (
+    <ul>
+      {todos.map(todo =>
+        <div key={todo.value}>
+            {todo.value}
+        </div>
+      )}
+    </ul>
+  )
+   const mapStateToProps = state => ({
+    todos: state.todos
+  })
+   export default connect(mapStateToProps)(TodoList)
