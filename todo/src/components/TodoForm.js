7039cbbdd61e0be7ff634446590365d3bb571aab
Lee Len Formento <leebatef@gmail.com>
2018-10-22 20:05:59 -0400

    Todo form

---
+++ todo/src/components/TodoForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TodoForm = props => {
+    return (
+        <div className="todoForm">
+            <form onSubmit={props.handleSubmit}>
+                <input
+                    type='text'
+                    placeholder='Add a Todo'
+                    value={props.value}
+                    onChange={props.handleChange}
+                />
+                <button type="submit">Add Todo</button>
+            </form>
+        </div>
+    );
+};
+
+export default TodoForm;
