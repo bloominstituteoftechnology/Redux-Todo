b87f919bab35664b6b5851ce858e5734c5f1bc46
Unknown <zachary.arney@gmail.com>
2018-10-23 15:44:28 -0500

    Import reducer for store/Redux dev tools thing

--- todo/src/index.js
+++ todo/src/index.js
@@ -8,8 +8,12 @@ import { Provider } from 'react-redux';
 import './index.css';
 
 import App from './App';
+import reducer from './reducers';
 
-const store = createStore();
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
