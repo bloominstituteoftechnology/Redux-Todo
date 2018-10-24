6a795f95c825de87c4e1d1264639d5afa0a17230
Gannon <gkleblanc@gmail.com>
2018-10-24 10:47:53 -0400

    was on wrong track, redoing some file structure and adding my store on index.js, but Provider is somehow missing from node-modules, so I'm about to do some investigating

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,32 +1,12 @@
 import React, { Component } from "react";
-
 import "./App.css";
+import ToDo from "./components/todo.js";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      heading: "Todo List:",
-      todos: [
-        {
-          value: "finish this lambda project",
-          completed: false
-        }
-      ],
-      todo: ""
-    };
-  }
-
-  changeTaskHandler = event => {
-    this.setState({ todo: event.target.value });
-  };
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h2>Remove This Title</h2>
-        </header>
+        <ToDo />
       </div>
     );
   }
