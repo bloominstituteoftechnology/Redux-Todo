1e2e814abaa4cd59e2c22d03c1cddb53157f3097
Gannon <gkleblanc@gmail.com>
2018-10-24 10:04:15 -0400

    added constructor and made edits to app.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,22 +3,29 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      heading: "Todo List:",
+      todos: [
+        {
+          value: "finish this lambda project",
+          completed: false
+        }
+      ],
+      todo: ""
+    };
+  }
+
+  changeTaskHandler = event => {
+    this.setState({ todo: event.target.value });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Remove This Title</h2>
         </header>
       </div>
     );
