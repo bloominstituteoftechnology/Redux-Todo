33aa5bc08ab034520cb1c70890e972b644537bba
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-10-24 19:59:18 -0400

    fixed reducers, no longer reloads initial state

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,38 +1,44 @@
-
-let idNum = 0
+let idNum = 0;
 const todos = [
-        {
-            value: "Example",
-            completed: false,
-            id: idNum++
-        },
-        {
-            value: "Example 2",
-            completed: false,
-            id: idNum++
-        }
-    ]
+  {
+    value: "Example",
+    completed: false,
+    id: idNum++
+  },
+  {
+    value: "Example 2",
+    completed: false,
+    id: idNum++
+  }
+];
 
-
-const itemReducer = (state = todos, action) => {
-    console.log("inreducer", action, state)
-    switch (action.type) {
-        case "ADD_TODO":
-        return [...todos, {value: action.payload, completed: false, id: idNum++}]
-        case "TOGGLE_TODO":
-        return todos.map(todo => {
-            if (todo.id === action.payload) {
-                todo.completed = !todo.completed
-                return todo
-            }  
-                return todo
-            
-        })
-        default: 
-        return state;
+const itemReducer = (
+  todos = [
+    {
+      value: "",
+      completed: false,
+      id: idNum++
     }
+  ],
+  action
+) => {
+  console.log("inreducer", action, state);
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...todos,
+        { value: action.payload, completed: false, id: idNum++ }
+      ];
+    case "TOGGLE_TODO":
+      return state.map(todo => {
+        if (todo.id === action.payload) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+    default:
+      return state;
+  }
+};
 
-}
-
-
-export default itemReducer
+export default itemReducer;
