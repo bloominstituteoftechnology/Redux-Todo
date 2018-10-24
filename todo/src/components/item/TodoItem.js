926112f79bb5897a7f90ff0ee4fef5b872183dc7
Unknown <zachary.arney@gmail.com>
2018-10-23 20:24:18 -0500

    Moved components into their own folders

--- todo/src/components/item/TodoItem.js
+++ todo/src/components/item/TodoItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TodoItem(props) {
   return (
-    <li onClick={() => props.clickHandler(props.todo)} >
+    <li onClick={() => props.clickHandler(props.todo)} completed={props.completed} >
       {props.todo.item}
     </li>
   )
