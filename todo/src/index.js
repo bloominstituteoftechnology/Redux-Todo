1d1fc1c17f47fde308a0eec70d9045bcf01b692e
Gannon <gkleblanc@gmail.com>
2018-10-24 11:58:31 -0400

    added store to index.js  and initialState into reducer

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./App";
+import reducer from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
