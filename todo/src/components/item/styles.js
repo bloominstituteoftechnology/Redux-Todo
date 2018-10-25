9246d12688d3a3650ece551b9e9a96b01b940a82
Unknown <zachary.arney@gmail.com>
2018-10-25 02:50:59 -0500

    Styling for markImportant functionality

--- todo/src/components/item/styles.js
+++ todo/src/components/item/styles.js
@@ -20,11 +20,25 @@ export const StyledItem = styled.li`
   ${(props) => {
     if (props.completed === 'true') {
       return `
-      text-decoration: line-through;
-      color: rgb(149, 199, 153);
-      list-style-type: disc;
+        text-decoration: line-through;
+        color: rgb(149, 199, 153);
+        list-style-type: disc;
+      `;
     }
-    `;
+  }};
+  ${(props) => {
+    if (props.important === 'true') {
+      return `
+        list-style-type: none;
+        ::before {
+          content: "!";
+          margin-right: 19.35px;
+          color: rgb(241, 94, 105);
+          font-weight: 900;
+          text-decoration: none;
+        }
+        }
+      `;
     }
   }};
   padding: 5px 10px;
@@ -33,12 +47,21 @@ export const StyledItem = styled.li`
 export const ImportantButton = styled.div`
   color: rgb(52, 61, 69);
   font-size: 2rem;
-  font-weight: 600;
+  font-weight: 900;
   background: rgb(100, 115, 125);
   padding: 2px 10px;
   border-radius: 10px;
+  ${(props) => {
+    if (props.important === 'true') {
+      return `
+          color: rgb(241, 94, 105);
+      `;
+    }
+  }};
   :hover {
     cursor: pointer;
-    background: rgb(241, 94, 105);
+    background: rgb(167, 173, 185);
   }
 `;
+
+// const importantMarkers = {}
