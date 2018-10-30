ffadf18eae1da55c4635af987992c2000c77ac1c
Colin Dismuke <colin.dismuke@gmail.com>
2018-10-29 21:33:43 -0500

    Created app and added Provider

---
+++ todo/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+
+ReactDOM.render(
+    <Provider store={createStore(function() { return {} })}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
