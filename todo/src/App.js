aafb5b6ea7970eee81d8e80b609270b149784621
Wanda <doubleuevans@gmail.com>
2018-10-30 15:32:13 -0500

    added class component, with state =  todoArray

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,19 +1,35 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 
 class App extends Component {
-
-  state={
-    todos: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoArray: [{
+          value: 'import redux library',
+          completed: false
+        },
+        {
+          value: 'build action',
+          completed: false
+        },
+        {
+          value: 'build action creator',
+          completed: false
+        }
+      ]
+    }
   }
-  
-  render() {
-    return (
-      <div className="App">
 
+  render() {
+    return ( 
+      <div className = "App">
 
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ todo/src/reducers/index.js
@@ -0,0 +1,9 @@
+import { ADD_TODO, TOGGLE_TODO } from '../actions';
+
+const initialState = {
+  todos: []
+}
+
+const newTodo = (state = initalState, action ) => {
+
+}
