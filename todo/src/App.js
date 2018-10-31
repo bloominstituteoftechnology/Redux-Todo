e8adb3960d2704ed684c7484ca34521a485f38e5
jaklundun <govi.escudero@gmail.com>
2018-10-31 03:54:40 -0700

    Minor copy tweak.

--- todo/src/App.js
+++ todo/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Todo</h1>
+        <h1>Todos</h1>
         <TodoList />
         <AddTodo />
       </div>
