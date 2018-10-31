cbd4880c9f7a64164e6987e8ff7f2d1722542b6b
Colin Dismuke <colin.dismuke@gmail.com>
2018-10-30 21:06:39 -0500

    Setup index.js

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import rootReducer from './reducers'
 
+import './index.css';
+import App from './components/App';
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-    <Provider store={createStore(function() { return {} })}>
+    <Provider store={store}>
         <App />
     </Provider>
 , document.getElementById('root'));
