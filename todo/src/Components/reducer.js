1fe66faf5e1ee614ad58655b716c6d2313070cab
Kendra Williams <kmonique.williams@gmail.com>
2018-11-01 19:54:50 -0400

    index.js completed, reducer.js initial set up

---
+++ todo/src/Components/reducer.js
@@ -0,0 +1,13 @@
+
+
+//import action strings 
+
+//set initial state to the empty todo array
+const initialState = { todo: []}
+
+export default (state = initialState, action) => {
+   switch(action.type){
+      default: 
+         return state
+   }
+}
