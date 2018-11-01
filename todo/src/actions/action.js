e7dd4da1aa30322c5075687fe7872eeb8f379683
mglittleton <michaelglittleton@gmail.com>
2018-11-01 00:13:06 -0400

    Basic redux framework complete
    Created action and reducer file. Wrapped everything in a Provider Component.

---
+++ todo/src/actions/action.js
@@ -0,0 +1,5 @@
+export const DELETE_TODO = 'DELETE_TODO';
+
+export const deleteTodo = () => {
+    return { type: 'DELETE_TODO'}
+}
---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,12 @@
+import {} from "../actions/action";
+
+const initialState = {
+  todos: []
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
