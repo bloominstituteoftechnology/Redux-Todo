6075fef2878a0da58eaea6e57c44d596db11a423
Emmanium <pradoe.ep@gmail.com>
2018-10-31 22:01:51 -0600

    did a bunch of more set-up

---
+++ todo/src/components/Todo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Todo = () => {
+  <h2>This is a todo.</h2>
+}
+
+export default Todo;
---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TodoList = () => {
+  <h3>this is the list</h3>
+}
+
+export default TodoList;
