76170325629e52790f02075efc55c632753e7c73
iepoch <staclem@live.com>
2018-10-31 18:34:07 -0700

    Added todo store

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import todo from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store =
+    createStore(
+        todo, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+
---
+++ todo/src/reducers/index.js
@@ -0,0 +1,16 @@
+
+
+const initialState = {
+    todo: []
+
+}
+
+
+export default (state = initialState, action) => {
+
+    switch (action.type) {
+        //case //Fill In:
+         //   return //Fill In
+
+    }
+}
