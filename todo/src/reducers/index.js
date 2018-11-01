163762a5442cf2817c01f6327740009f5bbfcb1a
Jennifer King <jenbellpgh@gmail.com>
2018-10-31 22:25:04 -0400

    Filled in more framework for redux by adding the cases to the reducer. Still defaulting to return state right now.

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,4 +1,4 @@
-
+import { ADD_TODO, TOGGLE } from '../actions';
 
 // Set the initial state
 const initialState = {
@@ -10,7 +10,13 @@ const initialState = {
 
 // Will need to put actions into a switch here.
 export default ( state = initialState, action ) => {
+  // I think I will need to use a copy of State to push additions onto.
+  // Setting this up now, so that I don't forget:
+  // let oldState = state.todos;
+
   switch( action.type ) {
+    case ADD_TODO:
+    case TOGGLE:
     default:
       return state;
   }
