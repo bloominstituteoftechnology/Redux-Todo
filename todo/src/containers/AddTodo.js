9d1e802482a7f73281193ebe42548034b715fea3
Rick Fleming <elfeming111@yahoo.com>
2018-11-01 20:59:33 -0700

    Adding styling to components

--- todo/src/containers/AddTodo.js
+++ todo/src/containers/AddTodo.js
@@ -1,7 +1,46 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import styled from 'styled-components';
 
 import {addTodo} from '../actions/actions';
+
+const FormContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-bottom: 20px;
+
+    form{
+        display: flex;
+        justify-content: center;
+        width: 50%
+    
+        .todo-input{
+            width: 80%;
+            height: 25px;
+            margin-right: 10px;
+            outline: none;
+            border: 1px solid black;
+            border-radius: 5px;
+        }
+    
+        .submit-button{
+            width: 20%;
+            color: black;
+            background-color: white;
+            border: 2px solid black;
+            border-radius: 5px;
+            outline: none;
+
+            &:hover{
+                color: white;
+                background-color: black;
+                cursor: pointer;
+            }
+        }
+    }
+    
+`;
 
 class AddTodo extends React.Component{
     constructor(props){
@@ -29,6 +68,9 @@ class AddTodo extends React.Component{
 
     handleSubmit = (event)=>{
         event.preventDefault();
+        if(this.state.value === ''){
+            return;
+        }
         this.props.addTodo(this.state.currentID, this.state.value);
         this.setState({
             currentID: this.state.currentID + 1,
@@ -38,10 +80,12 @@ class AddTodo extends React.Component{
 
     render(){
         return(
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" onChange={this.handleTextInput} placeholder="Enter todo item..." value={this.state.value}></input>
-                <input type="submit" value="Submit"></input>
-            </form>
+            <FormContainer>
+                <form onSubmit={this.handleSubmit}>
+                    <input className="todo-input" type="text" onChange={this.handleTextInput} placeholder=" Enter todo item..." value={this.state.value}></input>
+                    <input className="submit-button" type="submit" value="Submit"></input>
+                </form>
+            </FormContainer>
         )
     }
 }
