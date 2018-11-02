f368e467dfbe62955e0e50c0b9801a904214bb69
Julie Jonak <juliejonak10@gmail.com>
2018-11-02 10:12:58 -0700

    Add to do item functionality working"

--- todo/src/reducers/reducers.js
+++ todo/src/reducers/reducers.js
@@ -7,11 +7,16 @@ const reducer = (state = initialState, action) => {
     switch(action.type){
         case "ADD_ITEM":
             return Object.assign( 
-                {}, state, state.todos.concat([{
-                    id: action.id,
-                    value: action.value,
-                    completed: false }
-                ])
+                {}, state, {
+                    todos: [
+                        ...state.todos,
+                        {
+                            id: action.id,
+                            value: action.value,
+                            completed: false
+                        }
+                    ]
+                }
             )
         default:
             return state
