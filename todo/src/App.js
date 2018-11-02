88ca3ccc96c0b995b160bc904fd2f0ba1f25018e
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-11-01 23:03:00 -0600

    General Skeleton Structure Created, Ready to Import Data

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,24 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let todo = ['Ready','Set', 'Go']
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          {todo.map(item => {return <ul> <li><form> <input type='checkbox'></input></form></li> 
+          <h2>{item}</h2></ul>})}
+          Daily Tasks:<form><input type='text'></input></form>
       </div>
     );
   }
