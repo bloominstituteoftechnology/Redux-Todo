b7fb5769ef35f6c2f065667144d2c4f6b552979d
Tommy Collison <tommy@collison.ie>
2018-11-02 22:21:28 -0700

    wrapping in provider

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import {Provider} from '../node_modules/react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
