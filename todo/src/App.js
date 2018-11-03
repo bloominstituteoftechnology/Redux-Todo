ef556b4ba9ef66c393bfc37b4913be3c0756cdab
Rob Salzberg <robsalzberg@gmail.com>
2018-11-03 14:38:27 -0400

    Completed todo list MVP and added some reactstrap styling

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos';
 import TodoForm from './components/TodoForm';
+import { connect } from 'react-redux';
+import { addTodo } from './actions';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoText: ''
+    };
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  addTodo = () => {
+    const { todoText } = this.state;
+    const newTodo = {
+      id: this.props.todos.length + 1,
+      completed: false,
+      text: todoText
+    };
+    this.props.addTodo(newTodo);
+    this.setState({ todoText: '' });
+  };
+
   render() {
     return (
       <div className="App">
-        <TodoForm />
+        <TodoForm
+          handleInputChange={this.handleInputChange}
+          todoText={this.state.todoText}
+          addTodo={this.addTodo}
+        />
+        <div>
+          <Todos todos={this.props.todos} />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    todos: state.todos,
+  };
+};
+
+export default connect(mapStateToProps, { addTodo })(App);
--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const TodoForm = props => {
-  return (
-    <div>
-      <input
-        name="todoText"
-        value={props.todoText}
-        type="text"
-        onChange={props.handleInputChange}
-        placeholder="Add todo"
-      />
-      <button onClick={props.addTodo}>Add Todo</button>
-    </div>
-  );
+	return (
+		<div>
+			<Form>
+				<FormGroup>
+					<Label for="Add A Todo">Add a Todo </Label>
+					<div className="form">
+						<Input
+							name="todoText"
+							value={props.todoText}
+							type="text"
+							onChange={props.handleInputChange}
+							placeholder="Enter here"
+						/>
+						<Button color="primary" onClick={props.addTodo}>
+							Submit
+						</Button>
+					</div>
+				</FormGroup>
+			</Form>
+		</div>
+	);
 };
 
 export default TodoForm;
