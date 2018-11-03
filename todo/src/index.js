5790c736e8a61511fcea6bccceaa10f50ad2a4a0
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-11-03 12:39:16 -0400

    Add some files and connect initial store

--- todo/src/index.js
+++ todo/src/index.js
@@ -10,7 +10,7 @@ import todoList from './reducers';
 const store = createStore(todoList)
 
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>, document.getElementById('root'));
 
