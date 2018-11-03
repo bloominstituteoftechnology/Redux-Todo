e5b35af85156c37883e3a0dea3cbe619f0ef62b4
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-11-03 12:02:22 -0400

    added imports for redux and react-redux added store variable and wrapped App in provider component

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import todos from './reducers/index.js';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  todos,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>, document.getElementById('root'));
