d0821181eaeb426cdffd8437311a5049e4d58ca1
Mitchell Robles <mitchellr92@live.com>
2018-11-03 09:46:36 -0700

    setting up reducer

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+    todos: []
+}
+
+export default (state = initialState, action) => {
+    switch (actio.tpye) {
+        case:
+            return {}
+        case:
+            return {}
+        default:
+            return state;    
+    }
+}
