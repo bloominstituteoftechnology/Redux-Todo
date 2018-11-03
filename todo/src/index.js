9b491d137d82b17a4690e79682077fddbaf74ba0
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-11-03 13:51:53 -0500

    store has been set up and files are being set

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Reducer from './reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+ReactDOM.render(
+<Provider store={store}>    
+  <App />
+</Provider>, document.getElementById('root'));
 
 
 serviceWorker.unregister();
