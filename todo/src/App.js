941522e12a68157d04edcf13eb2de0c04464f6aa
Griffin <griffinshuth@gmail.com>
2018-11-03 10:21:15 -0600

    got everything set up

---
+++ todo/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <form>
+        <p>Griffin TODO LIST</p>
+          <input></input>
+        </form>
+        <button>SUBMIT</button>
+      </div>
+    );
+  }
+}
+
+export default App;
