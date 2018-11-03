4dc7282c1fc85c74df0229a036cff27a0465763e
Ben <bennybooftw@gmail.com>
2018-11-03 15:03:37 -0400

    Added some css for line-through on completed todos

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { completedToDo } from '../actions/actions';
+import '../../src/App.css';
 
 const Todo = props => {
   return(
     <ul>
-      <li onClick={() => props.completedToDo(props.todo.id)}>{props.todo.value}</li>
+      <li onClick={() => props.completedToDo(props.todo.id)} className={props.todo.completed ? "checked" : ''}>{props.todo.value}</li>
     </ul>
   )
 }
