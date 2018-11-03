92b75152d441e95a1f0219f19922c8b7d3827dd9
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-11-03 14:41:05 -0400

    completed the reducer

---
+++ todo/src/reducer/reducer.js
@@ -0,0 +1,20 @@
+import {UPDATE_COMPLETE, ADD_TODO }from '../action/action'
+
+const initialState = [];
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case UPDATE_COMPLETE:
+      const update = state.map(item => {
+        if(item.id === action.id) {
+          return {...item, completed: true}
+        }
+        return item
+      })
+      return update
+    case ADD_TODO:
+      return [...state, {...action.payload, id: state.length + 1}]
+    default: 
+      return state;
+  }
+}
