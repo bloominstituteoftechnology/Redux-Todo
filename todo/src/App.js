1087c03b116a7c3f864c0cab32fecee794c3e9f0
Henry Moland <henrymoland@gmail.com>
2018-11-02 22:43:47 -0700

    Created React App

---
+++ todo/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+  state = {
+    todos: [
+      {id: 1, content: 'Work on some code'},
+      {id: 2, content: 'Work out'},
+      {id: 3, content: 'Cook dinner'},
+      {id: 4, content: 'Catch up on TV shows'}
+    ]
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+
+      
+      </div>
+    );
+  }
+}
+
+export default App;
