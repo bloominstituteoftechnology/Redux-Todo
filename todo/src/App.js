aaa81c16d864f6b3300dba8a784a22ef87f43494
Louis <7.louislee@gmail.com>
2018-11-03 11:00:55 -0700

    form is working and added some styling

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { add } from './actions/actions'
 import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm';
 
 class App extends Component {
   render() {
@@ -10,6 +10,7 @@ class App extends Component {
         <header className="App-header">
           <h2>To-do List</h2>
           <TodoList />
+          <TodoForm />
         </header>
       </div>
     );
--- todo/src/index.js
+++ todo/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import reducer from "./reducers/reducers"
+import reducer from "./reducers/reducer"
 
 //creating store
 const store = createStore(reducer);
