e60fb19c618fe8d6f58c03f888a15bd24bdd3b60
Robin U Familara <robin.familara@cgius.com>
2018-11-03 10:24:00 -0500

    Modified main index at src -Created the necessary method for store creation -Created Provider for access to redux global store -Created todoReducers to represent state -Allowed createStore to pass information -Added extensions for Redux tests

--- todo/src/index.js
+++ todo/src/index.js
@@ -5,7 +5,13 @@ import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-const store = createStore();
+import { createStore } from 'redux'; // Redux store creation method
+import { Provider } from 'react-redux'; // Wrap around App component. Access to redux global store.
+ // creating store by passing information (reducer: representation of state) to createStore. 
+// Testing with Redux Chrome extension
+const store = createStore(todosReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ); 
 
  ReactDOM.render(
 <Provider store={store}>
