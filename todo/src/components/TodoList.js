033693cec4f250b58f0fb9e27c3811d1b7bd9b40
Colin Dismuke <colin.dismuke@gmail.com>
2018-11-02 22:37:28 -0500

    Finished TodoList component

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -8,14 +8,69 @@ class TodoList extends Component {
         this.state = {
             input: '',
         }
+    };
+
+    inputHandler = event => {
+        this.setState({ [event.target.name] : event.target.value });
+    }
+
+    toggleHandler = todoId => {
+        this.props.toggleTodo(todoId);
+    }
+
+    removeHandler = () => {
+        this.props.removeTodos();
+    }
+
+    addHandler = (event) => {
+        const { input } = this.state;
+        
+        const newTodo = {
+            input,
+            completed: false,
+            id: this.props.todo ? this.props.todo.length + input : 0 + input
+        }
+        
+        this.props.addTodo(newTodo);
+        this.setState({
+            input: ''
+        })
     }
 
     render() {
         return (
             <div>
                 <form>
-
+                    <input
+                        name="input"
+                        value={this.state.input}
+                        onChange={this.inputHandler}
+                    />
+                    <button 
+                        type="button"
+                        onClick={this.addHandler}
+                    >
+                        Add
+                    </button>
                 </form>
+                <ul>
+                    {this.props.todos.map( todo => {
+                        return (
+                            <li
+                                onClick={() => this.toggleHandler(todo.id)}
+                                key={todo.id}
+                                style={
+                                    todo.completed ?
+                                    { textDecoration: "line-through"} :
+                                    null
+                                }
+                            >
+                                {todo.input}
+                            </li>
+                        )
+                    })}
+                </ul>
+                <button onClick={() => this.removeHandler()}>Clear</button>
             </div>
         )
     }
