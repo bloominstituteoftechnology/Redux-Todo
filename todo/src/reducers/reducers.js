423af92eaf30a6d12b133f0d0332d8786258255f
tommycollison <tommy@collison.ie>
2018-11-05 13:20:13 -0800

    creating an action in reducers.js

--- todo/src/reducers/reducers.js
+++ todo/src/reducers/reducers.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import {UPDATE_NAME} from '../actions'
 
-const initialState = {name: 'Tommy' };
+// const initialState = {name: 'Tommy' };
+// export default (state = initialState, action) => {
+//     switch (action.type) {
+//         case UPDATE_NAME:
+//         return {
+//             ...state, 
+//             name: action.payload}
+//         default: return state
+//     }
+// }
+
+const initialState = {todo: []}
+
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case UPDATE_NAME:
-        return {
-            ...state, 
-            name: action.payload}
-        default: return state
+    switch (action.type) { 
+        default: return state;
     }
 }
-
-
