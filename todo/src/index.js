74f5cebf436ad89ba614833cfa4c1b09677bb8f7
cocoitali <cmburatto@gmail.com>
2018-11-08 21:17:01 -0800

    FIX (made changes to createStore)

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
+import rootReducer from "./actions";
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
