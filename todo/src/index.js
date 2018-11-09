84f039c5ecc7ea376ef80d2f7c137ac06aab496d
cocoitali <cmburatto@gmail.com>
2018-11-08 19:55:49 -0800

    MVP (created store)

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
