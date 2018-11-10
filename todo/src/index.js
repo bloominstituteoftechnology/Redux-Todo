619511d5d35003b6eb67eec424868d1fb2f5a98a
cocoitali <cmburatto@gmail.com>
2018-11-10 10:01:43 -0800

    FIX (made changes to store)

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import App from "./App";
-import rootReducer from "./components/App";
+import App from "./components/TodoApp";
+import rootReducer from "./reducers/todos";
 
 const store = createStore(rootReducer)
 
