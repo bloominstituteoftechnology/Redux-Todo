ccae376826b51c0dd4b7d06d38eea9600edde73e
cocoitali <cmburatto@gmail.com>
2018-11-10 10:53:29 -0800

    MVP (action creators)

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -2,15 +2,17 @@ export const ADD_TODO = "ADD_TODO";
 export const TOGGLE_TODO = "TOGGLE_TODO";
 export const DELETE_TODO = "DELETE_TODO";
 
-export const addTodo = () => {
+export const addTodo = (text) => {
   return {
-    type: ADD_TODO
+    type: ADD_TODO,
+    text
   };
 };
 
-export const toggleTodo = () => {
+export const toggleTodo = (id) => {
   return {
-    type: TOGGLE_TODO
+    type: TOGGLE_TODO,
+    id
   };
 };
 
