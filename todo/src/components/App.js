0ce826c13767f24185922d008c1bdcf981197e8e
cocoitali <cmburatto@gmail.com>
2018-11-11 12:49:02 -0800

    MVP (set up new components)

---
+++ todo/src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component } from 'react';
+
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <TodoForm />
+        </div>
+    )
+  }
+}
+
+export default App;
---
+++ todo/src/components/TodoForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TodoForm extends Component {
+  render() {
+    return (
+      <div>
+        </div>
+    )
+  }
+}
+
+export default TodoForm;
