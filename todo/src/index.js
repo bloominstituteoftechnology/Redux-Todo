afb0b4a830f45562f1ac87e21d3ef7df2b9b1f9f
erscriber <erscriber@gmail.com>
2018-11-13 23:05:21 -0500

    Created Reducer directory and index.js file

--- todo/src/index.js
+++ todo/src/index.js
@@ -5,6 +5,12 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+const store = createStore(() => {});
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
 
