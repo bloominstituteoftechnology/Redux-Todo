09bdb29f3e676292991c305d53dcdcd088ad7ec8
jheinz1999 <jonathan.heinz@aol.com>
2018-12-03 15:02:58 -0800

    started implementing react-redux

---
+++ redux-todo/src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+
+  todos: []
+
+};
+
+export default export default (state = initialState, action) => {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+};
