ff012c57b63e6d2b59f1013218757d45ede14f12
Trai Lynne <trailynne@gmail.com>
2018-12-04 16:20:39 -0700

    Gave App access to store Passed store as a prop of Provider Wrapped app in Provider

--- todo/src/index.js
+++ todo/src/index.js
@@ -17,7 +17,14 @@ import * as serviceWorker from './serviceWorker';
 // Store Definition
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
