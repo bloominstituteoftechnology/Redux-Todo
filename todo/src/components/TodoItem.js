0cfd692886636871a1580616b9a0c654032c27bf
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2018-12-04 15:38:23 -0800

    Add polish

--- todo/src/components/TodoItem.js
+++ todo/src/components/TodoItem.js
@@ -3,22 +3,19 @@ import "./TodoItem.css";
 
 const TodoItem = props => {
 	return (
-		<div className = "todo-item">
+		<div className='todo-item'>
 			<button
 				className='red'
 				onClick={() => props.removeTodo(props.todo.id)}>
 				(x)
 			</button>
-            <div
+			<div
 				className={
 					props.todo.completed ? "completed todo-list" : "todo-list"
 				}
-				onClick={() => props.toggleCompleted(props.todo.id)}
-				// id={props.todo.display ? null : 'display-none'}
-			>
+				onClick={() => props.toggleCompleted(props.todo.id)}>
 				{props.todo.value}{" "}
 			</div>
-			
 		</div>
 	);
 };
