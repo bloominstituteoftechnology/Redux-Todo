44536274f80c2a679512ef742951493d167bfc0c
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2018-12-04 16:09:05 -0600

    displays list and form

--- todo/src/components/App.js
+++ todo/src/components/App.js
@@ -1,17 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
+import { addTodo, toggleComplete } from '../actions/actions';
+import { connect } from 'react-redux';
 
 class App extends Component {
+  state = {
+    inputText:''
+  }
 
-  
+  handleChange = ev => {
+    this.setState({ [ev.target.name]: ev.target.value});
+  }
+
+  handleSubmit = ev => {
+    ev.preventDefault();
+    console.log('clicked');
+   
+  }
+
 
   render() {
     return (
       <div className="App">
-        
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="inputText"
+          onChange={this.handleChange}
+          placeholder="add a todo"
+          value={this.state.inputText}
+          autoComplete="off"
+        />
+        <button type="submit">add todo</button>
+        </form>
+        <div className="list">
+          {this.props.todos.map((todo,index) => {
+            return (<div className="todo">
+              <p key={index}>{todo.value}</p>
+              {todo.complete ? <h3>X</h3> : null}
+            </div>)
+          })}
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    todos: state.todos
+  };
+}
+
+const withState = connect(
+  mapStateToProps, 
+  {
+    addTodo, 
+    toggleComplete
+  });
+
+export default withState(App);
