de76ff8ccd925d5505090e3c24758ff79d8653ed
David Freitag <freitagdavid@outlook.com>
2018-12-04 17:02:16 -0500

    todo/src/components/TodoList: Refactored to class component

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Todo from './Todo';
 import shortid from 'shortid'
 
-const TodoList = props => {
-    return (
-        <ul>
-            {props.tasks.map(task => <Todo key={shortid.generate()} text={task.text} complete={task.complete} id={task.id} />)}
-        </ul>
-    );
+class TodoList extends Component {
+    render() {
+        return (
+            <ul>
+                {this.props.tasks.map(task => <Todo key={shortid.generate()} text={task.text} complete={task.complete} id={task.id} />)}
+            </ul>
+        )
+    }
 };
 
 export default TodoList;
