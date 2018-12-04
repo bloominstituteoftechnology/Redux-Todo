dc93bfb86f7ca218b0482f612b2f4e61b99f6f3d
David Freitag <freitagdavid@outlook.com>
2018-12-04 16:10:36 -0500

    todo/src/reducers/index: wrote add task

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,10 +1,29 @@
-export default (state = null, action) => {
+const initialState = {
+    tasks: [
+        {
+            id: '',
+            text: 'Starter Task',
+            completed: false
+        }
+    ]
+}
+
+export default (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_TASK':
-            return action.payload
+            return Object.assign({}, state, {
+                tasks: [
+                    ...state.tasks, {
+                        id: action.payload.id,
+                        text: action.payload.text,
+                        completed: false
+                    }]
+            })
         case 'COMPLETE_TASK':
             return action.payload
         case 'REMOVE_TASK':
             return action.payload
+        default:
+            return state
     }
 }
