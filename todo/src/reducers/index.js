7eb36de85bde662ec07f8629a0f16dc53233fe80
Trai Lynne <trailynne@gmail.com>
2018-12-04 16:33:03 -0700

    Listed starter actions in reducer Created ActionType exports Added empty cases to switch statement

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,5 +1,6 @@
 // ActionType Exports
-
+const ADD_TO_LIST = 'ADD_TO_LIST';
+const TOGGLE_COMPLETE = 'TOGGLE_COMPLETE';
 
 // Initial State 
 const initialState = {
@@ -9,6 +10,15 @@ const initialState = {
 // Reducer
 const reducer = (state = initialState, action) => {
     switch(action.type){
+        case ADD_TO_LIST:
+
+        case TOGGLE_COMPLETE:
+
+
+        // Stretch
+        // case DELETE_ITEM:
+
+        // case CLEAR_COMPLETE:
 
         default: 
             return state;
