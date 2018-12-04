b71d25f880e266a7c27022f53627f2173796eb34
ykrueng <yasirah.krueng@gmail.com>
2018-12-04 17:39:40 -0600

    add delete button to remove todo

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import {
   addTodo,
@@ -10,27 +11,53 @@ import {
 
 import TodoForm from './TodoForm';
 
+const TodosWrapper = styled.div`
+  .todo{
+    display: flex;
+    margin-bottom: 10px;
+    
+    .button {
+      margin-right: 10px;
+      border: 1px solid darkslategray;
+      text-align: center;
+      padding: 3px;
+      line-height: 8px;
+      border-radius: 5px;
+
+      &:hover {
+        cursor: pointer;
+        background-color: gray;
+        color: white;
+      }
+    }
+  }
+`;
+
 class TodoList extends React.Component {
   render() { 
     return (
-      <div>
+      <TodosWrapper>
         Todo App
         {
           this.props.todos.map((todo,id) => (
-            <div
-              key={id}
-              onClick={() => this.props.toggleStatus(id)}
-              style={{
-                textDecoration: todo.completed ? 'line-through' : 'none',
-                fontStyle: todo.completed ? 'italic' : 'normal'
-              }}
-            >
-              {todo.value}
+            <div className="todo">
+              <div className="button" onClick={() => this.props.deleteTodo(id)}>x</div>
+              <div
+                className="todo--value"
+                key={id}
+                onClick={() => this.props.toggleStatus(id)}
+                style={{
+                  textDecoration: todo.completed ? 'line-through' : 'none',
+                  fontStyle: todo.completed ? 'italic' : 'normal'
+                }}
+              >
+                {todo.value}
+              </div>
             </div>
           ))
         }
         <TodoForm handleSubmit={this.props.addTodo}/>
-      </div>
+      </TodosWrapper>
     );
   }
 }
