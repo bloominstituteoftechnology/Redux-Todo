ad8478f118ed4a4ed19210f8b5c29760b49958cb
Addison-Stavlo <AddieStavlo@gmail.com>
2018-12-04 18:32:55 -0500

    styles added

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { addTodo, toggleComplete, deleteTodo } from './actions';
+import styled from 'styled-components';
 
 class App extends Component {
   constructor() {
@@ -37,6 +38,7 @@ class App extends Component {
   addTheTodo = ev => {
     ev.preventDefault();
     this.props.addTodo(this.state.newTodo);
+    this.setState({newTodo: ''})
   }
 
   toggleComplete = index => {
@@ -49,11 +51,12 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <ListWrapper>
+        <h1 className='list-title'>My Todo List</h1>
         <form onSubmit={this.addTheTodo}>
           <input name='newTodo' value={this.state.newTodo} 
           placeholder='add todo item' onChange={this.handleChange}/>
-          <button type='submit'>Submit</button>
+          <button type='submit'>Add Item</button>
         </form>
 
         {this.props.todos.map((todo,index) => {
@@ -62,28 +65,116 @@ class App extends Component {
           <div className='todo-item'
             onClick={()=>this.toggleComplete(index)}
             key={todo.todo}
-            >{todo.todo}</div>
-          <div className='complete-status'>completed: {` ${todo.completed}`}</div>  
-          <button onClick={()=>this.deleteTodo(index)}>Delete</button>
+            >
+            <p className={todo.completed? 'completed-todo' : ''} >{todo.todo}</p>
+            <div className='complete-and-remove'>
+            <p className='complete-status'>{todo.completed? 'COMPLETED' : ''}</p>
+            {/* <button onClick={todo.completed? 
+              ()=>this.deleteTodo(index)
+              :
+              ()=>{alert('Todos must be complete before removal')
+              this.toggleComplete(index)}
+            }>
+                Remove Item</button> */}
+            {todo.completed ? 
+              <button onClick={()=>this.deleteTodo(index)}>Remove Item</button> 
+            : 
+              null
+            }
+            </div>
+          </div>
+          
           </>
           )
         })}
-      </div>
+      </ListWrapper>
     );
   }
 }
+const ListWrapper = styled.div`
+  border: 2px solid red;
+  border-radius: 15px;
+  box-shadow: 10px 10px 10px 0 lightgray;
+  margin: 10px auto;
+  width: 90%;
+  max-width: 700px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: darkred;
+  color: white;
 
+  form {
+    width: 90%;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+
+    input {
+      width: 80%;
+    }
+  }
+
+  .list-title {
+    text-decoration: underline;
+    font-style: italic;
+  }
+
+  .todo-item {
+    border: 2px solid white;
+    width: 90%;
+    padding: 10px;
+    border-radius: 10px;
+    cursor: pointer;
+    display: flex;
+    justify-content: space-between;
+    margin: 10px 0;
+  }
+  .todo-item:hover {
+    background: white;
+    color: darkred;
+    border: 2px solid red;
+  }
+  .todo-item:active {
+    background: darkred;
+  }
+  .todo-item p {
+    margin: 5px 10px;
+    font-weight: bold;
+  }
+  .completed-todo {
+    text-decoration: line-through;
+  }
+  .complete-and-remove {
+    display: flex;
+
+  }
+  button {
+    z-index: 3;
+  }
+
+  button:hover {
+    cursor: pointer;
+    background: red;
+    color: white;
+  }
+  button:active {
+    background: darkred;
+  }
+
+`
 function mapStateToProps(state) {
   return {
     todos: state.todos
   };
 } 
 
-function mapDispatchToProps(dispatch) {
-  return {
-    addTodo: text => dispatch(addTodo(text))
-  };
-}
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     addTodo: text => dispatch(addTodo(text))
+//   };
+// }
 
 const withState = connect(
   mapStateToProps,
