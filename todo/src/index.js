5b1850df826009c7742673cbe6552acf022f8514
wisdomtooth <cbtsao47@gmail.com>
2018-12-04 17:59:03 -0600

    add Redux

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducer";
+
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
