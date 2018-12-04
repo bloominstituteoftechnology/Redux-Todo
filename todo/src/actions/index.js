491ce29b0e7a1049befbe39ae39e85be93e46302
ykrueng <yasirah.krueng@gmail.com>
2018-12-04 16:05:59 -0600

    add action creators

---
+++ todo/src/actions/index.js
@@ -0,0 +1,26 @@
+// constants
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
+export const TOGGLE_STATUS = 'TOGGLE_STATUS';
+
+// actions creators
+export const addTodo = (newTodo) => ({
+  type: ADD_TODO,
+  payload: newTodo,
+});
+
+export const deleteTodo = (id) => ({
+  type: DELETE_TODO,
+  payload: id,
+})
+
+export const updateTodo = (updatedTodo, id) => ({
+  type: UPDATE_TODO,
+  payload: id,
+})
+
+export const toggleStatus = (id) => ({
+  type: TOGGLE_STATUS,
+  payload: id,
+})
