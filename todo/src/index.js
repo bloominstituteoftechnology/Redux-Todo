2b01a751d6efb0c551b5c3fdb4943def5d17a331
jamespagedev <jamespagedev@outlook.com>
2018-12-05 01:29:40 -0700

    (Stretch): localstorage working

--- todo/src/index.js
+++ todo/src/index.js
@@ -6,10 +6,19 @@ import './Style.css';
 import App from './App';
 import reducer from './reducers';
 
+let persistedState = localStorage.getItem('reduxState')
+  ? JSON.parse(localStorage.getItem('reduxState'))
+  : undefined;
+
 const store = createStore(
   reducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+});
 
 ReactDOM.render(
   <Provider store={store}>
