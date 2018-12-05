6090dbeb8020967e0c4154116069858241994ae9
jheinz1999 <jonathan.heinz@aol.com>
2018-12-04 16:16:50 -0800

    refactored persistence

--- redux-todo/src/reducers/index.js
+++ redux-todo/src/reducers/index.js
@@ -8,18 +8,21 @@ const initialState = (localStorage.todoList) ? JSON.parse(localStorage.todoList)
 };
 
 export default (state = initialState, action) => {
+
+  let newState = {};
+
   switch (action.type) {
 
     case ADD:
-      const newState = {todos: [...state.todos, action.payload]};
+      newState = {todos: [...state.todos, action.payload]};
       localStorage.todoList = JSON.stringify(newState);
       return newState;
     case TOGGLE:
-      const newState2 = {todos: state.todos.map(item =>{
+      newState = {todos: state.todos.map(item =>{
         return action.id === item.value ? {value: item.value, completed: !item.completed} : item;
       })};
-      localStorage.todoList = JSON.stringify(newState2);
-      return newState2;
+      localStorage.todoList = JSON.stringify(newState);
+      return newState;
     default:
       return state;
   }
