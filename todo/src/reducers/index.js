85f8f380c0069ef5e92914ecdf74b19c7e050ad1
Thomas Claydon <tomclaydon102@gmail.com>
2018-12-04 19:16:04 -0500

    Finished all Stretch Assignments

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,41 +1,45 @@
-export const ADD_TODO = 'ADD_TODO';
-export const TOGGLE_TODO = 'TOGGLE_TODO';
-export const DELETE_TODO = 'DELETE_TODO';
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const DELETE_TODO = "DELETE_TODO";
 
-const initialState = {
-  todos: [{todo: "Initial Test", completed: false}]
+const storedTodoState = JSON.parse(localStorage.getItem("todoState"));
+console.log(storedTodoState);
+
+let initialState = {
+  todos: [{ todo: "Initial Test", completed: false }]
 };
+if (storedTodoState) {
+  initialState = storedTodoState;
+}
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO:
-      return {
+      let newToDoState = {
         ...state,
         todos: [...state.todos, { todo: action.payload, completed: false }]
       };
+      localStorage.setItem("todoState", JSON.stringify(newToDoState));
+      return newToDoState;
     case TOGGLE_TODO:
       return {
         ...state,
-        todos: state.todos.map((item, index)=>{
-            if(index === action.payload){
-                return { ...item, completed: !item.completed};
-            }else{
-                return item;
-            }
+        todos: state.todos.map((item, index) => {
+          if (index === action.payload) {
+            return { ...item, completed: !item.completed };
+          } else {
+            return item;
+          }
         })
       };
     case DELETE_TODO:
       return {
-          ...state,
-          todos: state.todos.filter(item => {
-              return item.completed === false;
-          })
+        ...state,
+        todos: state.todos.filter(item => {
+          return item.completed === false;
+        })
       };
     default:
       return state;
   }
 };
-
-
-
-//action.payload.id
