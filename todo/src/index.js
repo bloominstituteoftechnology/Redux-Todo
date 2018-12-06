31aa976fe37ae688742e970924be0ef1a5d193d9
David Freitag <freitagdavid@outlook.com>
2018-12-06 09:52:14 -0500

    todo/src/index: Prettier

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducers from './reducers'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
 const store = createStore(reducers);
 
-store.subscribe(() => console.log(store.getState()))
+store.subscribe(() => console.log(store.getState()));
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>
-    , document.getElementById('root'));
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
