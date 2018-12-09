f1824c642385ba8c8fad8df5e17d44127a17e971
kmarker1101 <kmarker1101@icloud.com>
2018-12-09 12:09:30 -0600

    cleanup

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import todoReducer from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import todoReducer from "./reducers";
 
-import App from './App';
+import App from "./App";
 
-import './index.css';
+import "./index.css";
 
 const store = createStore(todoReducer);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
