0b5910a6da719e3297686dc7c738a967ab3a65a0
chrisgins <christiansgina@gmail.com>
2018-12-11 11:38:41 -0700

    trimmed down files to make a sleeker file structure

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import todo from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = 
+   createStore(
+       todo
+   )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+   ReactDOM.render(
+       <Provider store={store}>
+       <App />
+       </Provider>, document.getElementById('root')
+   );
