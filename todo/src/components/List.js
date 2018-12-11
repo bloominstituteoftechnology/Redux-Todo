447a512ae3eb900dd1cda068e7e74c1448a2af5d
Jarrad Miller <jm020801@gmail.com>
2018-12-11 16:47:44 -0700

    some bug I cant find

---
+++ todo/src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import Todo from './ToDo';
+ const List = (props) => {
+    return (
+        <div> {props.todo.map(item => ( <Todo key={item.id} item={item} />))} </div>    )
+}
+ const mapStatetoProps = (state) => {
+    return state;
+}
+ export default connect(mapStatetoProps)(List) 
