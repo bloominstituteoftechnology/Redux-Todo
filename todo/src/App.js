775201e334ab08721696d5bf42080ae875609126
David Situ <davidsitu626@gmail.com>
2018-12-11 17:40:16 -0800

    David Situ TodoList Complete, Button Complete, completeToggle pending

---
+++ todo/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Todos from './components/Todos';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Todos />
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
