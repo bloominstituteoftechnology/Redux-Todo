b30f81f985d4194ea6519cb39a33d43d8fba7bda
mcferrenm <mcferrenmax@gmail.com>
2019-01-28 17:26:32 -0500

    Add input and submit, controll input from Component state

--- todo/src/components/TodoListContainer.js
+++ todo/src/components/TodoListContainer.js
@@ -1,17 +1,46 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const TodoListContainer = props => {
-  return (
-    <div className="todo-list-container">
-      {props.todos.map(todo => (
-        <ul style={{ textDecoration: todo.completed && "line-through" }}>
-          {todo.value}
-        </ul>
-      ))}
-    </div>
-  );
-};
+class TodoListContainer extends React.Component {
+  state = {
+    todoInput: {
+      value: "",
+      completed: null
+    }
+  };
+
+  handleChange = event => {
+    event.persist();
+    this.setState(prevState => {
+      return {
+        todoInput: {
+          ...prevState.todoInput,
+          [event.target.name]: event.target.value
+        }
+      };
+    });
+  };
+  render() {
+    return (
+      <div className="todo-list-container">
+        <input
+          onChange={this.handleChange}
+          type="text"
+          placeholder="Enter todo..."
+          autoComplete="off"
+          name="value"
+          value={this.state.todoInput.value}
+        />
+        <button>Add todo</button>
+        {this.props.todos.map(todo => (
+          <ul style={{ textDecoration: todo.completed && "line-through" }}>
+            {todo.value}
+          </ul>
+        ))}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {
--- todo/src/index.js
+++ todo/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 
+import { rootReducer } from "./reducers";
 import "./index.css";
 import App from "./App";
-import { rootReducer } from "./reducers";
 
 const store = createStore(rootReducer);
 
