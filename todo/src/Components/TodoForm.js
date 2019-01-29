ca6389c4a74696891b4bccdbcb08efc593ac2523
Enoka Jaona <Ejaona@gmail.com>
2019-01-29 16:05:32 -0600

    created todo form component

---
+++ todo/src/Components/TodoForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TodoForm = () => (
+  <form>
+    <input type="text" name="todo" />
+    <input type="button" value="Add Todo" />
+  </form>
+);
+
+export default TodoForm;
