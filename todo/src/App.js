b1910782b1b66507651e6b8ec145b7cc50eeca38
umbur <work1415@gmail.com>
2019-01-29 15:52:52 -0700

    Added to do list with adding todo functionality

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
+import Todo from './components/Todo'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        
-          <p>
-           Initial commit
-          </p>
-
-        </header>
+        <Todo />
       </div>
     );
   }
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import reducer from "./reducers/index";
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
