933a4ef1fbfdebc7c9df3d8b3079099ddfd23876
umbur <work1415@gmail.com>
2019-01-29 15:00:30 -0700

    Initial commit, plus installed dependencies

---
+++ todo/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        
+          <p>
+           Initial commit
+          </p>
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
+// 
