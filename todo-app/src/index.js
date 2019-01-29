af7e6d3ead731d4df678769c3989b008da382718
leiannel <leilouis112@gmail.com>
2019-01-29 15:59:51 -0500

    initial commit

---
+++ todo-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+
+import './index.css';
+import App from './App';
+
+const store = createStore(rootReducer)
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
