c9a686f1a0bc178636d86d2ac04dd828170b249c
CJ <CJ@CJs-MacBook.local>
2019-01-30 14:40:08 -0600

    misc

--- todo/src/index.js
+++ todo/src/index.js
@@ -4,12 +4,23 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import rootReducer from './reducers'
+import styled from 'styled-components'
+
+const div = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  width: 300px;
+  height: 550px;
+
+`
+
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 render(
   <Provider store={store}>
-    <App />
+   <div> <App /></div>
   </Provider>,
   document.getElementById('root')
 )
