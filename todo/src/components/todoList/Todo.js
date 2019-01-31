2a344571fcca6a1d78a22fe95e080587f6aedddb
120356aa <yurlovandrew@gmail.com>
2019-01-30 18:36:23 -0800

    trying to add line through visualization

--- todo/src/components/todoList/Todo.js
+++ todo/src/components/todoList/Todo.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Todo = props => {
   return (
-    <li onClick={() => props.toggleCompleted(props.index)}>
+    <li 
+      onClick={() => props.toggleCompleted(props.index)}
+      style={{ textDecoration: props.index.completed ? "line-through" : "none" }}>
       {props.todo.value}
     </li>
   );
