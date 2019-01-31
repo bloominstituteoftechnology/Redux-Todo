7f0eaf53f427ab9d6e8f66d029199a0671666e36
kingmetri <demetribethel@gmail.com>
2019-01-30 21:46:22 -0500

    Created store and reducer. Added connect to App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import connect from 'react-redux';
 
 class App extends Component {
   render() {
@@ -25,4 +26,12 @@ class App extends Component {
   }
 }
 
-export default App;
+
+const mapStateToProps = 
+state =>{
+  return{
+    state
+  }
+}
+
+export default connect(mapStateToProps)(App);
