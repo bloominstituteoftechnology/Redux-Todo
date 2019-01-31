f643772a12c05132f8936eb515f1ee4693b87e61
Kseniya Platonava <ksu2008@gmail.com>
2019-01-31 01:05:02 -0500

    MVP Done

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -5,7 +5,7 @@ const Todo = props => {
   console.log(props)
 return (
   
-  <h2>className={props.todos.completed ? 'completed' : 'not yet' } onClick={() => props.toggleTodo(props.todo.todo)}>{props.todo.todo}</h2>
+  <h2 className={props.todo.completed ? 'completed' : 'not yet'} onClick={( ) => props.toggleTodo(props.todo.todo)}>{props.todo.todo}</h2>
 )
 }
 export default Todo;
