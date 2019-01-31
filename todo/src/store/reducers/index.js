04a9328956122006802fb3d748161789fab5a0a7
120356aa <yurlovandrew@gmail.com>
2019-01-30 17:13:24 -0800

    created actions and imported reducers

---
+++ todo/src/store/reducers/index.js
@@ -0,0 +1,28 @@
+import { ADD_TODO, TOGGLE_TODO } from '../actions';
+
+const initialState = {
+  todos: []
+};
+
+export default (state = initialState, action) => {
+
+  switch (action.type) {
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: [...state.todos, action.payload]
+      });
+
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo, index) => {
+          if (index === action.payload) {
+            return { ...todo, completed: !todo.completed };
+          }
+          return todo;
+        })
+      });
+      
+    default:
+      return state;
+  }
+};
