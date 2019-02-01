4ae55d3dc03ce20cc0a3fc10f2cc0d3849d6fe66
kingmetri <demetribethel@gmail.com>
2019-01-31 22:35:30 -0500

    Added state with ToDos.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
 
 class App extends Component {
-  render() {
+    state = {
+      todos: [
+        {id: 1, content: 'Code some more.'},
+        {id: 2, content: 'Play Call of Duty.'}
+      ]
+    }
+  
+  
+  render() 
+  {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1 className='center blue-text'>Todo's</h1>
+      <Todos todos={this.state.todos} />
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ todo/src/Todos.js
@@ -0,0 +1,18 @@
+//functional component
+import React from 'react';
+
+const Todos = ({todos}) => {
+    const todoList = todos.length ? (todos.map(todo =>{ return(
+        <div className='collection-item' key={todo.id}>
+        <span>{todo.content}</span>
+        </div> 
+        
+    )})) : (<p>You have no Todos</p>)   
+    return (
+        <div className="todos collection">
+                {todoList}
+        </div>
+    )
+}
+
+export default Todos;
