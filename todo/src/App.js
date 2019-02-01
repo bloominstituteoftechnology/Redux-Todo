c1b14c8d8b641e71c59a66a940ffb116d4f7a631
kingmetri <demetribethel@gmail.com>
2019-01-31 22:45:11 -0500

    added delete functionality.

--- todo/src/App.js
+++ todo/src/App.js
@@ -9,13 +9,21 @@ class App extends Component {
       ]
     }
   
-  
+  deleteTodo = (id) => {
+    console.log(id);
+const todos = this.state.todos.filter(todo => {
+  return todo.id !== id
+})
+this.setState({
+  todos
+})
+  }
   render() 
   {
     return (
-      <div className="App">
+      <div className="todo-app container">
       <h1 className='center blue-text'>Todo's</h1>
-      <Todos todos={this.state.todos} />
+      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
