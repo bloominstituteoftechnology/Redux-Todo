267f6fe6ab8efea858ef94f4471d0d5e03995955
Jonathan Dorety <kyullog@gmail.com>
2019-02-02 14:01:59 -0500

    Add props for Todo map

--- todo/src/Components/List.js
+++ todo/src/Components/List.js
@@ -3,20 +3,25 @@ import { connect } from "react-redux";
 
 import Todo from "./Todo";
 
+function List(props) {
+  return (
+    <div className="list">
+      {props.todos.map(entry => (
+        <Todo
+          entry={entry.todo}
+          complete={entry.complete}
+          key={entry.id}
+          id={entry.id}
+        />
+      ))}
+    </div>
+  );
+}
+
 const mapStateToProps = state => {
   return {
     todos: state.todos
   };
 };
-
-function List(props) {
-  return (
-    <div className="list">
-      {props.todos.map(entry => (
-        <Todo entry={entry.todo} id={entry.todo} />
-      ))}
-    </div>
-  );
-}
 
 export default connect(mapStateToProps)(List);
