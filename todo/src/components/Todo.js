ca453610f5c6bf1b37cd5caf1da174ff4a8f7ae2
david-chua <dp_chua@hotmail.com>
2019-02-02 15:01:42 -0500

    removed unnecessary ternary operator

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -7,7 +7,7 @@ import { deleteTodo, toggleTodo } from '../actions/actions';
 const Todo = (props) => {
   return (
     <div>
-      <div className={`todo-list ${props.todo.completed ? "true" : ""}`}>
+      <div className={`todo-list ${props.todo.completed}`}>
         <h3 onClick={() => props.toggleTodo(props.todo.name)} > {props.todo.name}</h3>
       </div>
       <button onClick={() => props.deleteTodo(props.todo.name)}> Delete Todo </button>
