1a3aca406b144d72f4e5094bdb761118820ad995
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-02-02 11:36:05 -0500

    ADD PROPS AND CREATETODO EVENT, EXPORT FORM

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const TodoForm = props => {
-    return(
+import { addTodo } from './../actions';
 
-        <div>
-            <form onSubmit={}>
-                <input></input>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
+class TodoForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: ''
+        }
+    }
+
+    createNewTodo = e => {
+        e.preventDefault();
+        this.props.addTodo(this.state.inputValue);
+    }
+    render() {
+        return(
+
+            <div>
+                <form onSubmit={this.createNewTodo}>
+                    <input></input>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }    
 }
+
+const mstp = state => {
+    return {}
+}
+
+export default connect(mstp, { addTodo: addTodo })(TodoForm)
