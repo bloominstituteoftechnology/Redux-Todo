aa8a6996d372816f713aebdadd03ce13f8deb384
Bao Pham <ryanvuepham@gmail.com>
2019-02-03 18:01:01 -0600

    creating actions

---
+++ todo/src/actions/index.js
@@ -0,0 +1,14 @@
+export const addTask = (task) => {
+    return {
+        type:'ADD_TASK',
+        payload:task
+    }
+},
+deleteTask = (taskId) => {
+    return {
+        type:'DELETE_TASK',
+        paydload:taskId
+    };
+};
+
+export default {addTask, deleteTask};
