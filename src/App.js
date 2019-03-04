7f802ccfc1de3c4f28c3038872910d6a44fe22f9
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-04 13:58:26 -0800

    Created TodoList Component Scaffold

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import TodoList from './components/TodoList';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <TodoList />
       </div>
     );
   }
---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is a class</h1>
+            </div>
+        )
+    }
+}
+
+export default TodoList
