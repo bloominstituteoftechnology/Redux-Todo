bc258708fba097d3e360cd2f5f140787d5ad80a6
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-04 13:21:15 -0800

    Created store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+
+const store = createStore(() => { });
+
 
 ReactDOM.render(
     <Provider store={store}>
