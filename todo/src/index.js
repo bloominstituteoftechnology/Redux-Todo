59aae63e4c23f97a5972b1a6b25ec521a56b8ca5
Craig Bourne <craigbourne15@gmail.com>
2019-03-05 16:20:14 +0000

    checking store in redux devtools

--- todo/src/index.js
+++ todo/src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 
 import todo from './reducers';
 
-const store = createStore(todo);
+const store = createStore(todo, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
 	<Provider store={store}>
