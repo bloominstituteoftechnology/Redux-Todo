c744ffcfc39e73777185c160554e912cd64b8f8c
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-05 12:39:44 -0800

    Created toggle filtered todos action type, action creator, and action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,5 @@
-export { ADD_TODO, addTodo, TOGGLE_TODO_COMPLETE, toggleTodoComplete } from './todoActions';
+export {
+    ADD_TODO, addTodo,
+    TOGGLE_TODO_COMPLETE, toggleTodoComplete,
+    TOGGLE_FILTERED_TODOS, toggleFilteredTodos
+} from './todoActions';
--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -1,5 +1,4 @@
 export const ADD_TODO = 'ADD_TODO';
-
 export const addTodo = (todo) => {
     return {
         type: ADD_TODO,
@@ -8,10 +7,16 @@ export const addTodo = (todo) => {
 }
 
 export const TOGGLE_TODO_COMPLETE = 'TOGGLE_TODO_COMPLETE';
-
 export const toggleTodoComplete = id => {
     return {
         type: TOGGLE_TODO_COMPLETE,
         payload: id
+    }
+}
+
+export const TOGGLE_FILTERED_TODOS = 'TOGGLE_FILTERED_TODOS';
+export const toggleFilteredTodos = () => {
+    return {
+        type: TOGGLE_FILTERED_TODOS
     }
 }
