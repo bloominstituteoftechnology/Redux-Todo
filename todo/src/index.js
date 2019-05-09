23ba6b434564d09650d6bcc9bacb451bd55ef1d0
Nathan <s6nculve@gmail.com>
2019-05-08 17:46:21 -0700

    Imported Redux Provider.

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
