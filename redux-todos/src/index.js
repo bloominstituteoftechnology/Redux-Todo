30586b77835141f9035b9c281bcb478bd263b1dc
Moses Samuel <mosamuel98@gmail.com>
2019-05-14 14:03:45 -0700

    wrap app with context provider connected to the store

--- redux-todos/src/index.js
+++ redux-todos/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './index.css';
+
+import rootReducer from './reducers'
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={createStore(rootReducer)}>
+        <App /> 
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
