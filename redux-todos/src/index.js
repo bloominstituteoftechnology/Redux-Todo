a1378314ae7ca215c48f7b8c0d0a621af6509582
Moses Samuel <mosamuel98@gmail.com>
2019-05-14 15:44:51 -0700

    refactor: remove Redux's provider

--- redux-todos/src/index.js
+++ redux-todos/src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 
 import './index.css';
 
-import rootReducer from './store/reducers'
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Provider store={createStore(rootReducer)}>
-        <App /> 
-    </Provider>,
+    <App />, 
     document.getElementById('root')
 );
 
