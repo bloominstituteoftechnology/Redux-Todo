19bf5c5076a2b64724761c55098660953dcb30cc
Moses Samuel <mosamuel98@gmail.com>
2019-05-14 14:00:30 -0700

    set up basic root reducer w/ initial state

---
+++ redux-todos/src/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+    todos: [
+        'figure out how to use Redux\'s new hooks API', 
+        'master slap bass',
+        'go to Kinnard\'s crypto exchange meetup this evening'
+    ]
+}
+
+export default function rootReducer(state=initialState, action) {
+    // switch(action.type) {
+        
+    // }
+    return state
+}
