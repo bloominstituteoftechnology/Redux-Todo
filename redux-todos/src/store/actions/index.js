b2ea22cbf25046dd42e60c2917f189f21d0dafb7
Moses Samuel <mosamuel98@gmail.com>
2019-05-14 14:18:04 -0700

    add action creators

---
+++ redux-todos/src/store/actions/index.js
@@ -0,0 +1,22 @@
+import * as C from '../constants'
+
+export function addTodo(todo) {
+    return {
+        type: C.ADD_TODO,
+        payload: todo
+    }
+}
+
+export function editTodo(todo) {
+    return {
+        type: C.ADD_TODO,
+        payload: todo
+    }
+}
+
+export function deleteTodo(todo) {
+    return {
+        type: C.DELETE_TODO,
+        payload: todo
+    }
+}
