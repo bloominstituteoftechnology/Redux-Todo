2b86a1524d3999556811cfbd88454ab8f03e243a
Moses Samuel <mosamuel98@gmail.com>
2019-05-14 14:33:39 -0700

    render TodoForm inside App

--- redux-todos/src/App.js
+++ redux-todos/src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.css';
 
 import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
 
 function App(props) {
   return (
     <div className="App">
       <h2>Yet Another "Todo List" App</h2>
+      <TodoForm />
       <TodoList />
     </div>
   );
