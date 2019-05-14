9dd890fae94b181bc763f6a0a32632926a50ad24
Moses Samuel <mosamuel98@gmail.com>
2019-05-14 15:47:06 -0700

    refactor: make a custom context the main provider

--- redux-todos/src/App.js
+++ redux-todos/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { createContext } from 'react';
 
 import './App.css';
 
 import TodoList from './components/TodoList'
 import TodoForm from './components/TodoForm'
 
+import initialState from './initialState.json'
+
+
+export const TodoContext = createContext(null)
+
 function App(props) {
+
+  
   return (
     <div className="App">
       <h2>Yet Another "Todo List" App</h2>
-      <TodoForm />
-      <TodoList />
+      <TodoContext.Provider value={initialState.todos}>
+        <TodoForm />
+        <TodoList />
+      </TodoContext.Provider>
     </div>
   );
 }
