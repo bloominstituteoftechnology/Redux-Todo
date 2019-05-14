285e8b679dd992b3ab2f80ae5bb7c332948b840a
Moses Samuel <mosamuel98@gmail.com>
2019-05-14 15:55:50 -0700

    handle "ADD_TODO" actions in rootReducer

--- redux-todos/src/store/reducers/index.js
+++ redux-todos/src/store/reducers/index.js
@@ -1,8 +1,10 @@
+import * as C from '../constants'
 import initialState from '../../initialState.json'
 
-export default function rootReducer(state=initialState, action) {
-    // switch(action.type) {
-        
-    // }
-    return state
+export default function todoReducer(state=initialState, action) {
+    switch(action.type) {
+        case C.ADD_TODO:
+            return state.concat(action.payload)
+        default: return state
+    }
 }
