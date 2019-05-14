4f51d050515f5c261a034b14928b573d66ab1703
Moses Samuel <mosamuel98@gmail.com>
2019-05-14 16:13:11 -0700

    use useReducer and pass its dispatch to TodoForm

--- redux-todos/src/App.js
+++ redux-todos/src/App.js
@@ -1,23 +1,24 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
 
 import './App.css';
 
 import TodoList from './components/TodoList'
 import TodoForm from './components/TodoForm'
 
+import rootReducer from './store/reducers'
 import initialState from './initialState.json'
 
 
 export const TodoContext = createContext(null)
 
 function App(props) {
-
+  const [todos, dispatchTodos] = useReducer(rootReducer, initialState)
   
   return (
     <div className="App">
       <h2>Yet Another "Todo List" App</h2>
       <TodoContext.Provider value={initialState}>
-        <TodoForm />
+        <TodoForm dispatch={dispatchTodos} />
         <TodoList />
       </TodoContext.Provider>
     </div>
