c8911ecba428d71d8fd79c9603b79033a6cb2a7d
Moses Samuel <mosamuel98@gmail.com>
2019-05-14 23:08:23 -0700

    TodoItem now renders a checkbox and a label

--- redux-todos/src/components/TodoItem.js
+++ redux-todos/src/components/TodoItem.js
@@ -1,12 +1,26 @@
 import React from 'react'
 
 export default function TodoItem(props) {
-    console.log(props)
-    return (
-        <li>
-            <div>
-                {props.item.content}
-            </div>
-        </li>
-    )
+// const dispatch = useContext(TodoContext)
+
+//   function handleChange() {
+//         dispatch({
+//             type: props.item.complete ? 'UNDO_TODO' : 'DO_TODO',
+//             id: todo.id,
+//         })
+//     }
+
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={props.item.complete}
+        //   onChange={handleChange}
+        />
+        {/* TODO (irony unintended): change API from "content" to "task" */}
+        {props.item.content}
+      </label>
+    </li>
+  )
 }
