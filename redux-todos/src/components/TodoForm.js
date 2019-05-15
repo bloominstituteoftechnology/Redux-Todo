3f72b17ee9cba5e6124789a550998bb229bbb73d
Moses Samuel <mosamuel98@gmail.com>
2019-05-14 19:17:30 -0700

    generate unique ids on each todo submission

--- redux-todos/src/components/TodoForm.js
+++ redux-todos/src/components/TodoForm.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
+import uuid from 'uuid'
+
 import { addTodo } from '../store/actions'
+
 
 
 export default function TodoForm(props) {
@@ -8,7 +11,7 @@ export default function TodoForm(props) {
     function handleSubmit(evt) {
         evt.preventDefault()
         props.dispatch(addTodo({
-            id:69,
+            id: uuid(),
             content:newTodo,
             completed:false
         }))
