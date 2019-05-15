a7d3545fe1de2bf0cdbe70e853e6d5ca1d9b00e6
Moses Samuel <mosamuel98@gmail.com>
2019-05-15 11:51:08 -0700

    add three new constants for filtering todos

--- redux-todos/src/store/constants.js
+++ redux-todos/src/store/constants.js
@@ -1,3 +1,9 @@
+// Todos
 export const ADD_TODO = "ADD_TODO"
 export const EDIT_TODO = "EDIT_TODO"
 export const DELETE_TODO = "DELETE_TODO"
+
+// Filter
+export const SHOW_ALL = "SHOW_ALL"
+export const SHOW_COMPLETE = "SHOW_COMPLETE"
+export const SHOW_INCOMPLETE = "SHOW_INCOMPLETE"
