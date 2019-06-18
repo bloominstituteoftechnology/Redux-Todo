8b01aa9edfd247d4918fab537fb33c95cc5ecd71
Dave Irwin <david@redirwin.com>
2019-06-18 16:56:06 -0700

    Removed combined reducers from reducers/index.js. Set up component imports into App.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import AddItem from "./components/AddItem";
+import ShowIncomplete from "./components/ShowIncomplete";
+import ShowComplete from "./components/ShowComplete";
 
 import "./App.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <AddItem />
+      <ShowIncomplete />
+      <ShowComplete />
+    </div>
+  );
 }
 
 export default App;
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,14 +1,8 @@
-import { combineReducers } from "redux";
-import AddItem from "./add-item";
-import DeleteItem from "./delete-item";
-import ShowIncomplete from "./show-incomplete";
-import ShowComplete from "./show-complete";
-
 const rootReducer = combineReducers({
-  addItem: AddItem,
-  deleteItem: DeleteItem,
   activeMovies: ShowIncomplete,
-  completedMovies: ShowComplete
+  completedMovies: ShowComplete,
+  addItem: AddItem
+  //   deleteItem: DeleteItem,
 });
 
 export default rootReducer;
