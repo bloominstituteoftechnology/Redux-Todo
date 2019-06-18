23e525535d3f9c93c32acdf0162cfd8273b8b8da
LadyKerr <kadiekerr@gmail.com>
2019-06-18 17:28:34 -0400

    action creators

---
+++ todo/src/actions/index.js
@@ -0,0 +1,13 @@
+export const ADD_NEW_TASK = 'ADD_NEW_TASK';
+export const addNewTask = () => {
+  return {
+    type: ADD_NEW_TASK
+  }
+}
+
+export const TOGGLE_TASK = 'TOGGLE_TASK';
+export const toggleTask = () => {
+  return {
+    type: TOGGLE_TASK
+  }
+}
