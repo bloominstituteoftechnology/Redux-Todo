cd5e449bc2b05db6d4b133b185157e8f6a7c5972
LadyKerr <kadiekerr@gmail.com>
2019-06-18 17:34:02 -0400

    modifications

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <TodoList />
     </div>
   );
 }
--- todo/src/index.js
+++ todo/src/index.js
@@ -8,7 +8,10 @@ import rootReducer from './reducers';
 import './index.css';
 import App from './App';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 ReactDOM.render(
   <Provider store={store}>
