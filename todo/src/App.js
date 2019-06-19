928385a3e13e13a43fae2b83d83a87277bffe530
Dave Irwin <david@redirwin.com>
2019-06-18 18:09:40 -0700

    Commented out currently unused components.

--- todo/src/App.js
+++ todo/src/App.js
@@ -9,8 +9,10 @@ function App() {
   return (
     <div className="App">
       <AddItem />
-      <IncompleteList />
-      <CompleteList />
+      {/*   
+    <IncompleteList />
+    <CompleteList /> 
+    */}
     </div>
   );
 }
