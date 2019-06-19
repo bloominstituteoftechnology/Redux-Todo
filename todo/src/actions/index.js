0daf63d126869e057c07e34c5ffc71ecf9a1115d
Dave Irwin <david@redirwin.com>
2019-06-18 19:22:25 -0700

    Removed seperate action handlers created in error.

--- todo/src/actions/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { addItem } from "./addItem";
-import { toggleDone } from "./toggleDone";
-
-export default combineReducers({
-  addItem,
-  toggleDone
-});
