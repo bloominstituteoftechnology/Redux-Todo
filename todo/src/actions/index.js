b7a1360f221315dc48e70a9d05b192987eb73987
Dave Irwin <david@redirwin.com>
2019-06-18 19:27:04 -0700

    Created empty action handlers for testing.

---
+++ todo/src/actions/index.js
@@ -0,0 +1,10 @@
+export const ADD_ITEM = "ADD_ITEM";
+export const TOGGLE_DONE = "TOGGLE-DONE";
+
+export const addItem = newItem => {
+  console.log(newItem);
+};
+
+export const toggleDone = index => {
+  console.log(index);
+};
