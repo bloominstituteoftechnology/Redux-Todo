21bf86efdd5af856bdc527dd63abba53e7026de5
fritzgt <tuanis@live.com>
2019-06-21 22:47:43 -0400

    Created store and wrapped app with provider

--- todo/src/index.js
+++ todo/src/index.js
@@ -9,4 +9,15 @@ import { createStore } from "redux";
 //Connecting redux with react
 import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//importing reducer
+import reducer from "./reducers";
+
+//Creating store
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
