8fd48740b4fcc0b921b79ec18f9a4745133eafec
Becheru888 <becheru93@gmail.com>
2019-06-25 14:34:36 +0100

    Reduce and combine reduce done!

--- todo/src/index.js
+++ todo/src/index.js
@@ -4,8 +4,19 @@ import App from './App';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
+import { todoReducer} from './Reducers/reducers';
+
+const combinedReducer = combineReducers({
+    todos: todoReducer
+  });
+
+const store = createStore(
+ combinedReducer,
+ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-<Provider>
+<Provider store={store}>
     <App />
 </Provider>,
 document.getElementById('root')
