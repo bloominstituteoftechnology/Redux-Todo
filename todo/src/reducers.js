4afabd1a2e333e98d99cb26632e0f7929a2a1f54
Allie Robinson <allierob93@gmail.com>
2019-07-12 19:09:51 -0700

    Action creators made & ready to test.

--- todo/src/reducers.js
+++ todo/src/reducers.js
@@ -1,11 +1,11 @@
-import { ADDTODOS, DELETETODOS } from '../actions';
+import { ADDTODOS, DELETETODOS } from './actions';
 
 // The initial state of the todo list is just an empty string.
 const initialState = {
     todo: ''
 }
 
-// These reducers will either add a todo or delete the list of todos 
+// This reducer will either add a todo or delete the list of todos 
 export default (state = initialState, action) => {
     switch (action.type) {
         case ADDTODOS: 
