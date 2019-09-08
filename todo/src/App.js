2f99aa4e5d724c0ae8eea110e4585a74ee083c84
Dil Sombayeva <dilyasom@gmail.com>
2019-09-08 12:34:30 -0700

    add header

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Todo from './components/Todo';
+import Header from './Header';
 
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <Todo />
-    );
-  }
+ render() {
+  return (
+   <div>
+    <Header />
+    <Todo />
+   </div>
+  );
+ }
 }
 
 export default App;
